3e731f7042ab5c60768cf78d2cd174dea8ca7475
Douglas Duteil <douglasduteil@gmail.com>
2019-03-21 15:19:58 +0100

    test: make jest-preset-angular work

--- packages/frontend/jest.config.js
+++ packages/frontend/jest.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   "preset": "jest-preset-angular",
-  "setupFilesAfterEnv": ["./src/setupJest.ts"],
-  globals: {
-    'ts-jest': {
-      tsConfig: './src/tsconfig.spec.json'
-    }
-  }
+  "setupFilesAfterEnv": ["./src/setupJest.ts"]
 };
