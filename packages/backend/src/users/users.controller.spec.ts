379055e9df4465e578ee057f491982fcb4b31c83
Douglas Duteil <douglasduteil@gmail.com>
2019-05-20 16:23:37 +0200

    test(backend): flatten controller tests

--- packages/backend/src/users/users.controller.spec.ts
+++ packages/backend/src/users/users.controller.spec.ts
@@ -1,18 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('Users Controller', () => {
-  let controller: UsersController;
-
-  beforeEach(async () => {
+  it('should be defined', async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [{ provide: UsersService, useValue: {} }]
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
-  });
-
-  it('should be defined', () => {
+    const controller = module.get<UsersController>(UsersController);
     expect(controller).toBeDefined();
   });
 });
