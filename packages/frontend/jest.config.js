5a9a8995d953c5ae09719e6879b541401d2a3784
Yassine <riffi.yassine@gmail.com>
2019-07-04 01:14:37 +0200

    fix: erreur jest

--- packages/frontend/jest.config.js
+++ packages/frontend/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   collectCoverageFrom: ["src/app/**/*.{html,ts}"],
   preset: "jest-preset-angular",
-  setupFilesAfterEnv: ["./src/setupJest.ts"],
-  testEnvironment: "jsDom"
+  setupFilesAfterEnv: ["./src/setupJest.ts"]
 };
