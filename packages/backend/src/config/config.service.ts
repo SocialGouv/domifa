d1253ee07e58e1b5eb0b974696d6eb043ff2e21c
pYassine <riffi.yassine@gmail.com>
2019-10-23 15:38:27 +0200

    fix: backend test

--- packages/backend/src/config/config.service.ts
+++ packages/backend/src/config/config.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from "@nestjs/common";
 import * as dotenv from "dotenv";
-import * as fs from "fs";
-import * as path from "path";
 
 @Injectable()
 export class ConfigService {
