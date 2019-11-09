00f6c446ded907bd41a334756c445059423b149c
Douglas Duteil <douglasduteil@gmail.com>
2019-11-09 01:38:28 +0100

    test(frontend): mock MatomoInjector#init fn

--- packages/frontend/src/app/app.component.spec.ts
+++ packages/frontend/src/app/app.component.spec.ts
@@ -3,7 +3,7 @@ import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
 import { async, TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
-import { MatomoModule } from "ngx-matomo";
+import { MatomoInjector, MatomoModule } from "ngx-matomo";
 import { AppComponent } from "./app.component";
 
 describe("AppComponent", () => {
@@ -15,6 +15,14 @@ describe("AppComponent", () => {
         HttpClientTestingModule,
         MatomoModule,
         RouterTestingModule
+      ],
+      providers: [
+        {
+          provide: MatomoInjector,
+          useValue: {
+            init: jest.fn()
+          }
+        }
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     }).compileComponents();
