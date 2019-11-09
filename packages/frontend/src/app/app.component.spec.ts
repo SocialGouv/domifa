4023128de94b5d78d704478fbb89163ca7a17d87
Douglas Duteil <douglasduteil@gmail.com>
2019-11-09 01:14:32 +0100

    test(frontend): add MatomoModule

--- packages/frontend/src/app/app.component.spec.ts
+++ packages/frontend/src/app/app.component.spec.ts
@@ -3,13 +3,19 @@ import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
 import { async, TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
+import { MatomoModule } from "ngx-matomo";
 import { AppComponent } from "./app.component";
 
 describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [RouterTestingModule, HttpClientModule, HttpClientTestingModule],
+      imports: [
+        HttpClientModule,
+        HttpClientTestingModule,
+        MatomoModule,
+        RouterTestingModule
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
