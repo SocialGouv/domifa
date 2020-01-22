f84d418f1b560ed45acf8c9ab38d7c180d03206a
pYassine <riffi.yassine@gmail.com>
2020-01-22 19:26:13 +0100

    fix(users): variable manquante dans l'interface

--- packages/frontend/src/app/modules/users/interfaces/user.ts
+++ packages/frontend/src/app/modules/users/interfaces/user.ts
@@ -1,30 +1,32 @@
 import { Structure } from "../../structures/structure.interface";
 
 export class User {
-  public id: string;
-  public nom: string;
-  public prenom: string;
   public email: string;
   public fonction: string;
-  public role: string;
+  public id: string;
+  public lastLogin: Date;
+  public nom: string;
   public password: string;
   public phone: string;
-  public token: string;
-  public structureId: number;
+  public prenom: string;
+  public role: string;
   public structure: Structure;
-  public lastLogin: Date;
+  public structureId: number;
+  public token: string;
+  public verified: boolean;
 
   constructor(user?: any) {
-    this.prenom = (user && user.prenom) || null;
-    this.nom = (user && user.nom) || null;
-    this.password = "";
     this.email = (user && user.email) || null;
     this.fonction = (user && user.fonction) || null;
-    this.role = (user && user.role) || null;
-    this.phone = (user && user.phone) || null;
-    this.token = (user && user.token) || null;
-    this.structureId = (user && user.structureId) || null;
     this.id = (user && user.id) || null;
+    this.nom = (user && user.nom) || null;
+    this.password = "";
+    this.phone = (user && user.phone) || null;
+    this.prenom = (user && user.prenom) || null;
+    this.role = (user && user.role) || null;
+    this.structureId = (user && user.structureId) || null;
+    this.token = (user && user.token) || null;
+    this.verified = (user && user.verified) || false;
     this.structure =
       (user && new Structure(user.structure)) || new Structure({});
     this.lastLogin = (user && new Date(user.lastLogin)) || null;
