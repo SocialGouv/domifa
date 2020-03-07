827a3c174e98b299b529f189c058776e17968ce4
Yassine R <riffi.yassine@gmail.com>
2020-03-07 09:47:27 +0100

    fix: tests

--- packages/frontend/src/app/modules/usagers/components/raft/raft.component.spec.ts
+++ packages/frontend/src/app/modules/usagers/components/raft/raft.component.spec.ts
@@ -1,7 +1,10 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { APP_BASE_HREF } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { UsagersModule } from "../../usagers.module";
 import { RaftComponent } from "./raft.component";
 
@@ -12,7 +15,12 @@ describe("RaftComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [],
-      imports: [UsagersModule],
+      imports: [
+        UsagersModule,
+        NgbModule,
+        HttpClientModule,
+        HttpClientTestingModule
+      ],
       providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
