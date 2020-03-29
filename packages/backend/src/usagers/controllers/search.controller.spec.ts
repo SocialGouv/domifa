cb9c8a323a939019de41a7c94ae7343cdc23856d
Yassine R <riffi.yassine@gmail.com>
2020-03-30 00:38:12 +0200

    test: fix erreurs

--- packages/backend/src/usagers/controllers/search.controller.spec.ts
+++ packages/backend/src/usagers/controllers/search.controller.spec.ts
@@ -1,5 +1,13 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { SearchController } from "./search.controller";
+import { DatabaseModule } from "../../database/database.module";
+import { forwardRef } from "@nestjs/common";
+import { UsersModule } from "../../users/users.module";
+import { StructuresModule } from "../../structures/structure.module";
+import { UsagersModule } from "../usagers.module";
+import { InteractionsModule } from "../../interactions/interactions.module";
+import { StatsService } from "../../stats/stats.service";
+import { StatsProviders } from "../../stats/stats-providers";
 
 describe("Search Controller", () => {
   let controller: SearchController;
@@ -7,6 +15,14 @@ describe("Search Controller", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SearchController],
+      imports: [
+        DatabaseModule,
+        forwardRef(() => UsersModule),
+        forwardRef(() => StructuresModule),
+        forwardRef(() => UsagersModule),
+        forwardRef(() => InteractionsModule),
+      ],
+      providers: [StatsService, ...StatsProviders],
     }).compile();
 
     controller = module.get<SearchController>(SearchController);
