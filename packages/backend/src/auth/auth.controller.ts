b05e4a42ec4bb5a6a683a39f3a898640605742f2
Yassine R <riffi.yassine@gmail.com>
2020-05-07 05:45:30 +0200

    fix(dashboard): ajout d'un id manquant

--- packages/backend/src/auth/auth.controller.ts
+++ packages/backend/src/auth/auth.controller.ts
@@ -66,7 +66,11 @@ export class AuthController {
   @UseGuards(AuthGuard("jwt"))
   @Get("domifa")
   public isDomifa(@Response() res: any, @CurrentUser() user: User) {
-    if (!user || user === null || user.structureId !== 28) {
+    if (
+      !user ||
+      user === null ||
+      (user.structureId !== 28 && user.structureId !== 1)
+    ) {
       return res.status(HttpStatus.UNAUTHORIZED).json({});
     }
     return res.status(HttpStatus.OK).json({});
