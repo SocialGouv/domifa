94a5b59c153ab549b0e6a16010b8879c946bd6ee
Nicolas Toublanc <n.toublanc@gmail.com>
2020-09-22 09:33:16 +0200

    feat(dev): cross-origin compatibility #788

--- packages/frontend/src/environments/environment.ts
+++ packages/frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: "http://localhost:3000/",
+  apiUrl: "http://localhost:4200/", // "proxy.conf.json" configuration will be used to proxy API requests to backend bypassing cross-origin requests security on firefox
   production: false,
 };
