1fa47ceabd016d8531938e3fbd55112ea888b78b
Nicolas Toublanc <n.toublanc@gmail.com>
2020-10-06 18:03:33 +0200

    fix(config): fix boolean config (#881)

--- packages/backend/src/config/config.service.ts
+++ packages/backend/src/config/config.service.ts
@@ -30,26 +30,7 @@ export class ConfigService {
   constructor() {
     dotenv.config();
     this.envConfig = {
-      DB_HOST: process.env.DB_HOST,
-      DB_PASS: process.env.DB_PASS,
-      DB_PORT: process.env.DB_PORT,
-      DB_USER: process.env.DB_USER,
-      DB_AUTH_SOURCE: process.env.DB_AUTH_SOURCE,
-      FILES_IV: process.env.FILES_IV,
-      FILES_PRIVATE: process.env.FILES_PRIVATE,
-      DOMIFA_FRONTEND_URL: process.env.DOMIFA_FRONTEND_URL,
-      DOMIFA_BACKEND_URL: process.env.DOMIFA_BACKEND_URL,
-      MJ_APIKEY_PRIVATE: process.env.MJ_APIKEY_PRIVATE,
-      MJ_APIKEY_PUBLIC: process.env.MJ_APIKEY_PUBLIC,
-      SECRET: process.env.SECRET,
-      UPLOADS_FOLDER: process.env.UPLOADS_FOLDER,
-      DOMIFA_ENV_ID: process.env.DOMIFA_ENV_ID || "",
-      DOMIFA_MONGOOSE_DEBUG: process.env.DOMIFA_MONGOOSE_DEBUG,
-      DOMIFA_SWAGGER_ENABLE: process.env.DOMIFA_SWAGGER_ENABLE,
-      DOMIFA_ADMIN_EMAIL: process.env.DOMIFA_ADMIN_EMAIL,
-      DOMIFA_MAILJET_FROM_EMAIL: process.env.DOMIFA_MAILJET_FROM_EMAIL,
-      DOMIFA_TIPIMAIL_FROM_EMAIL: process.env.DOMIFA_TIPIMAIL_FROM_EMAIL,
-      DOMIFA_CRON_ENABLED: process.env.DOMIFA_CRON_ENABLED,
+      ...process.env,
     };
     this.checkConfig();
   }
@@ -59,7 +40,7 @@ export class ConfigService {
   }
 
   public getBoolean(key: DomifaConfigKey): boolean {
-    const value = this.get[key];
+    const value = this.envConfig[key];
     return !!value && value.trim() === "true";
   }
 
