23abe992a4ecf740b91ededed3f34d232adfcb1c
Nicolas Toublanc <n.toublanc@gmail.com>
2020-10-13 15:59:12 +0200

    feat(Sécurité): revue de sécurité sur les endpoint API #855

--- packages/backend/src/auth/guards/admin.guard.ts
+++ packages/backend/src/auth/guards/admin.guard.ts
@@ -6,10 +6,6 @@ export class AdminGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
 
   public canActivate(context: ExecutionContext): boolean {
-    const roles = this.reflector.get<string[]>("roles", context.getHandler());
-    if (!roles) {
-      return true;
-    }
     const request = context.switchToHttp().getRequest();
     const user = request.user;
     return user && user.role === "admin";
--- packages/backend/src/auth/guards/domifa.guard.ts
+++ packages/backend/src/auth/guards/domifa.guard.ts
@@ -10,10 +10,6 @@ export class DomifaGuard implements CanActivate {
   ) { }
 
   public canActivate(context: ExecutionContext): boolean {
-    const roles = this.reflector.get<string[]>("roles", context.getHandler());
-    if (!roles) {
-      return true;
-    }
     const request = context.switchToHttp().getRequest();
     const user = request.user;
 
