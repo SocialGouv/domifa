63869dda52b17fbd458b03f78ab27bd4240aa315
Nicolas Toublanc <n.toublanc@gmail.com>
2020-11-10 17:08:50 +0100

    debt(mongo): add postgres backup script

--- _scripts/db/restore-mongo-tests.sh
+++ _scripts/db/restore-mongo-tests.sh
@@ -7,13 +7,19 @@ echo "# START RESET TRAVIS DB..."
 echo "#############################################################################"
 echo ""
 
+DB_USER=test
+DB_PASS=test
+DB_NAME=domifa_tests
+MONGO_DUMP_PATH=dump_tests.mongo.gz
+
 mongo domifa_tests --eval "db.runCommand( { dropAllUsersFromDatabase: 1, writeConcern: { w: 'majority' } } )"
 # sleep 2
 mongo domifa_tests --eval "db.dropDatabase()"
 # sleep 2
-mongo domifa_tests --eval "db.createUser({user:'test', pwd:'test', roles:[{role:'readWrite', db:'domifa_tests'}] });"
+mongo domifa_tests --eval "db.createUser({user:'${DB_USER}', pwd:'${DB_PASS}', roles:[{role:'readWrite', db:'${DB_NAME}'}] });"
 # sleep 2
-mongorestore --gzip --archive=dump_tests.mongo.gz
+mongorestore --gzip --archive=${MONGO_DUMP_PATH}
+
 # sleep 2
 
 echo ""
--- _scripts/db/restore-postgres-travis-entrypoint.sh
+++ _scripts/db/restore-postgres-travis-entrypoint.sh
@@ -6,7 +6,8 @@ echo "# RESTORE POSTGRES TRAVIS DB..."
 echo "#############################################################################"
 echo ""
 
-pg_restore --username=${POSTGRES_USERNAME} --role=${POSTGRES_USERNAME} --no-owner --exit-on-error --verbose --dbname=${POSTGRES_DATABASE} /app/_scripts/db/dump_tests.postgres.dump
+POSTGRES_DUMP_PATH=/app/_scripts/db/dump_tests.postgres.dump
+pg_restore --username=${POSTGRES_USERNAME} --role=${POSTGRES_USERNAME} --no-owner --exit-on-error --verbose --dbname=${POSTGRES_DATABASE} ${POSTGRES_DUMP_PATH}
 
 echo ""
 echo "#############################################################################"
