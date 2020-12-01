ab797f2e286f61ccc8bbbec25877fd9353e6b35c
Yassine R <riffi.yassine@gmail.com>
2020-12-01 16:17:58 +0100

    fix: async manquants (#1013)

--- packages/backend/src/mails/services/usagers-mails.service.ts
+++ packages/backend/src/mails/services/usagers-mails.service.ts
@@ -86,7 +86,7 @@ export class UsagersMailsService {
       .toPromise();
   }
 
-  public hardReset(user: AppUserForAdminEmail, token: string) {
+  public async hardReset(user: AppUserForAdminEmail, token: string) {
     const post = {
       to: [
         {
