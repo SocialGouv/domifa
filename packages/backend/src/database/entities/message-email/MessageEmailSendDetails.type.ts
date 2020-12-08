f348a76237451ca52820c2b24b1643ecf0476548
Nicolas Toublanc <n.toublanc@gmail.com>
2020-12-08 10:39:24 +0100

    feat(email): better batchs & email management #1015

--- packages/backend/src/database/entities/message-email/MessageEmailSendDetails.type.ts
+++ packages/backend/src/database/entities/message-email/MessageEmailSendDetails.type.ts
@@ -3,4 +3,5 @@ import { MessageEmailRecipient } from "./MessageEmailRecipient.type";
 export type MessageEmailSendDetails = {
   sent: MessageEmailRecipient[];
   skipped: MessageEmailRecipient[];
+  serverResponse: any;
 };
