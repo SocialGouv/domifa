1175a4d8cce617def40c7932578f3f75752a4542
toub <nicolas@toublanc.org>
2020-12-14 17:57:13 +0100

    feat(CREATION_DOM): ajout information langue parlée #993

---
+++ packages/frontend/src/app/shared/constants/AppLanguage.type.ts
@@ -0,0 +1,4 @@
+export type AppLanguage = {
+  isoCode: string; // ISO 639-2
+  label: string; // LABEL FR
+};
---
+++ packages/frontend/src/app/shared/constants/DEPARTEMENTS_MAP.const.ts
@@ -0,0 +1,22 @@
+// constante dupliquée côté frontend
+// https://fr.wikipedia.org/wiki/Liste_des_d%C3%A9partements_fran%C3%A7ais
+
+import { REGIONS_DEF } from "./REGIONS_DEF.const";
+
+const map: {
+  [key: string]: {
+    departmentName: string;
+    regionCode: string;
+    regionName: string;
+  };
+} = {};
+export const DEPARTEMENTS_MAP = REGIONS_DEF.reduce((acc, region) => {
+  region.departements.forEach((dep) => {
+    acc[dep.departementCode] = {
+      departmentName: dep.departementName,
+      regionCode: region.regionCode,
+      regionName: region.regionName,
+    };
+  });
+  return acc;
+}, map);
