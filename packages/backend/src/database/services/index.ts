25c1d604631a8f05a5b22a9e761d21e9fadb71cf
Nicolas Toublanc <nicolas@toublanc.org>
2020-12-15 16:07:24 +0100

    fix(stats): remove orphan stats (#1061)

--- packages/backend/src/database/services/index.ts
+++ packages/backend/src/database/services/index.ts
@@ -2,7 +2,7 @@ export * from "./app-user";
 export * from "./app-user-mail";
 export * from "./message-email";
 export * from "./monitoring";
+export * from "./structure-stats";
 export * from "./_mongo";
 export * from "./_postgres";
 export * from "./_scripts";
-
---
+++ packages/backend/src/database/services/structure-stats/index.ts
@@ -0,0 +1,1 @@
+export * from "./structureStatsRepository.service";
