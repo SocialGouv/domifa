2e4428220e7738fcbf4610e5c2e108c8dbd94ecc
toub <nicolas@toublanc.org>
2021-02-24 12:07:29 +0100

    bug(PP_COURRIERS): réception et remise de courrier/colis/adp #1195

--- packages/backend/src/_migrations/1614089579788-clear-etapeDemande.migration.ts
+++ packages/backend/src/_migrations/1614089579788-clear-etapeDemande.migration.ts
@@ -1,7 +1,11 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
+import { appLogger } from "../util";
 
 export class manualMigration1614089579788 implements MigrationInterface {
+  name = "manualMigration1614089579788";
+
   public async up(queryRunner: QueryRunner): Promise<void> {
+    appLogger.debug(`[Migration] UP "${this.name}"`);
     await queryRunner.query(
       `UPDATE usager SET "etapeDemande" = 5 where "etapeDemande" = 6;`
     );
