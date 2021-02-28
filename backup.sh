bf9a7db0a535910c733805f14e111cd0c0c1b8a2
toub <nicolas@toublanc.org>
2021-02-28 19:29:51 +0100

    fix(backup): fix mongo backup

--- backup.sh
+++ backup.sh
@@ -10,16 +10,22 @@ fi
 
 month_year=$(date "+%Y-%m")
 today=$(date "+%Y-%m-%d-%H-%M")
-base_dir=/mnt/database/backup-${DOMIFA_ENV_ID}-${month_year}/backup-${today}
-mkdir -p $base_dir
+BASE_DUMP_DIR=/mnt/database/backup-${DOMIFA_ENV_ID}-${month_year}/backup-${today}
+mkdir -p $BASE_DUMP_DIR
 
-MONGO_DUMP_PATH=${base_dir}/mongo_mongodump-${today}.gzip
+MONGO_DUMP_PATH=${BASE_DUMP_DIR}/mongo_mongodump-${today}.gzip
 POSTGRES_DUMP_NAME=postgres.pg_dump-${today}.postgres.custom.gz
-POSTGRES_DUMP_PATH=${base_dir}/${POSTGRES_DUMP_NAME}
+POSTGRES_DUMP_PATH=${BASE_DUMP_DIR}/${POSTGRES_DUMP_NAME}
+
+echo ""
+echo "BASE_DUMP_DIR: $BASE_DUMP_DIR"
+echo "MONGO_DUMP_PATH: $MONGO_DUMP_PATH"
+echo "POSTGRES_DUMP_PATH: $POSTGRES_DUMP_PATH"
+echo ""
 
 if [ "$DOMIFA_ENV_ID" == "prod" ]; then
     DOCKER_COMPOSE_PROJECT_NAME=master
-    (set -x && sudo docker exec ${DOCKER_COMPOSE_PROJECT_NAME}_mongo_1 sh -c 'mongodump --archive --gzip' > ${MONGO_DUMP_PATH})
+    (set -x && sudo docker exec ${DOCKER_COMPOSE_PROJECT_NAME}_mongo_1 sh -c "mongodump -u \$MONGO_INITDB_ROOT_USERNAME -p \$MONGO_INITDB_ROOT_PASSWORD --archive --gzip --db domifa" > ${MONGO_DUMP_PATH})
 else
     DOCKER_COMPOSE_PROJECT_NAME="$DOMIFA_ENV_ID"
     (set -x && sudo docker exec ${DOCKER_COMPOSE_PROJECT_NAME}_mongo_1 sh -c "mongodump -u \$MONGO_INITDB_ROOT_USERNAME -p \$MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase=admin --archive --gzip" > ${MONGO_DUMP_PATH})
