4ca798215c5ae0e80d34783593c73f0870628906
Yassine R <riffi.yassine@gmail.com>
2021-03-02 16:18:00 +0100

    Séparation des dossiers

---
+++ packages/backend/src/_common/model/message-sms/index.ts
@@ -0,0 +1,5 @@
+export * from "./MessageSms.type";
+export * from "./MessageSmsId.type";
+export * from "./SuiviSmsErrors.type";
+export * from "./MessageSmsUpdate.type";
+export * from "./MessageSmsStatus.type";
