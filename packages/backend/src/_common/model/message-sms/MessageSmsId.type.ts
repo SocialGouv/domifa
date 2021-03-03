1d6ce37f3e431738e265c071620528dd19f61413
Yassine R <riffi.yassine@gmail.com>
2021-03-03 10:41:09 +0100

    Ajout des metas pour les sms

--- packages/backend/src/_common/model/message-sms/MessageSmsId.type.ts
+++ packages/backend/src/_common/model/message-sms/MessageSmsId.type.ts
@@ -1,4 +1,4 @@
+import { MessageSmsReminders } from ".";
 import { InteractionType } from "../interaction";
-import { MessageSmsRappels } from "./MessageSmsRappels.type";
 
-export type MessageSmsId = InteractionType | MessageSmsRappels;
+export type MessageSmsId = InteractionType | MessageSmsReminders;
--- packages/backend/src/_common/model/message-sms/index.ts
+++ packages/backend/src/_common/model/message-sms/index.ts
@@ -2,4 +2,7 @@ export * from "./MessageSms.type";
 export * from "./MessageSmsId.type";
 export * from "./SuiviSmsErrors.type";
 export * from "./MessageSmsUpdate.type";
+export * from "./MessageSmsReminders.type";
+export * from "./MessageSmsInteractionMetas.type";
+export * from "./MessageSmsReminderMetas.type";
 export * from "./MessageSmsStatus.type";
