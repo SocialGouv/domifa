e5ff2d4ba3786646e9b2ad3f061c2f072b3d5ec2
Yassine R <riffi.yassine@gmail.com>
2021-04-13 07:17:23 +0200

    fix: message d'erreur quand on est déjà connecté fix(phone): correction de l'affichage du téléphone portable

--- packages/frontend/src/app/guards/logged-guard.ts
+++ packages/frontend/src/app/guards/logged-guard.ts
@@ -5,6 +5,7 @@ import {
   Router,
   RouterStateSnapshot,
 } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
 
 import { Observable, of } from "rxjs";
 import { map } from "rxjs/operators";
@@ -13,7 +14,11 @@ import { AuthService } from "../modules/shared/services/auth.service";
 
 @Injectable({ providedIn: "root" })
 export class LoggedGuard implements CanActivate {
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private notifService: ToastrService
+  ) {}
 
   public canActivate(
     route: ActivatedRouteSnapshot,
@@ -25,7 +30,14 @@ export class LoggedGuard implements CanActivate {
 
     return this.authService.isAuth().pipe(
       map((isAuth: boolean) => {
-        return !isAuth;
+        if (isAuth) {
+          this.notifService.error(
+            "Vous êtes déjà connecté, impossible d'accéder à cette page"
+          );
+          this.router.navigate(["/manage"]);
+          return false;
+        }
+        return true;
       })
     );
   }
