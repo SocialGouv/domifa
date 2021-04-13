a58101ac1c86f6904afba1655074fa228498806a
Yassine R <riffi.yassine@gmail.com>
2021-04-13 13:05:15 +0200

    fix(tests): correctin du test de la navbar

--- packages/frontend/src/app/modules/general/components/navbar/navbar.component.spec.ts
+++ packages/frontend/src/app/modules/general/components/navbar/navbar.component.spec.ts
@@ -1,6 +1,13 @@
+import { CommonModule, APP_BASE_HREF } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { RouterTestingModule } from "@angular/router/testing";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { SharedModule } from "../../../shared/shared.module";
 
 import { NavbarComponent } from "./navbar.component";
 
@@ -11,8 +18,18 @@ describe("NavbarComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [NavbarComponent],
-      imports: [RouterTestingModule],
-      providers: [],
+      imports: [
+        CommonModule,
+        BrowserModule,
+        SharedModule,
+        BrowserAnimationsModule,
+        FontAwesomeModule,
+        RouterTestingModule,
+        HttpClientModule,
+        HttpClientTestingModule,
+      ],
+      providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
+
       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
     }).compileComponents();
   }));
