e55a0e1388e0d5eeee7cf01e9c64ddee2be4e5ec
Yassine R <riffi.yassine@gmail.com>
2021-04-14 11:51:00 +0200

    init(redesign): nouveau système d'enregistrement de courrier

---
+++ packages/frontend/src/app/modules/usagers/components/set-interaction-form/set-interaction-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-set-interaction-form',
+  templateUrl: './set-interaction-form.component.html',
+  styleUrls: ['./set-interaction-form.component.css']
+})
+export class SetInteractionFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
