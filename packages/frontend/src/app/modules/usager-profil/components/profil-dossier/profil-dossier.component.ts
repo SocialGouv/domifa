6330585800df4b9ef6af84ee2eba6e56aef3d1ec
Yassine R <riffi.yassine@gmail.com>
2021-05-10 10:30:20 +0200

    feat(profil): ajout d'un nouveau module dédié au profil

---
+++ packages/frontend/src/app/modules/usager-profil/components/profil-dossier/profil-dossier.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profil-dossier',
+  templateUrl: './profil-dossier.component.html',
+  styleUrls: ['./profil-dossier.component.css']
+})
+export class ProfilDossierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- packages/frontend/src/app/modules/usager-profil/usager-profil-routing.module.ts
+++ packages/frontend/src/app/modules/usager-profil/usager-profil-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { ProfilCourriersComponent } from "./components/profil-courriers/profil-courriers.component";
+import { ProfilDossierComponent } from "./components/profil-dossier/profil-dossier.component";
+import { ProfilHistoriqueComponent } from "./components/profil-historique/profil-historique.component";
 import { ProfilOverviewComponent } from "./components/profil-overview/profil-overview.component";
 
-const routes: Routes = [{ path: ":id", component: ProfilOverviewComponent }];
+const routes: Routes = [
+  { path: "dossier/:id", component: ProfilDossierComponent },
+  { path: "historique/:id", component: ProfilHistoriqueComponent },
+  { path: "courriers/:id", component: ProfilCourriersComponent },
+  { path: ":id", component: ProfilOverviewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
