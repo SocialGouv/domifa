23163072cb430895a5f24db43846f20f50031512
Yassine R <riffi.yassine@gmail.com>
2021-05-10 10:59:57 +0200

    fix(tests): correction de tests manquants

--- packages/frontend/src/app/modules/usager-profil/components/profil-historique/profil-historique.component.spec.ts
+++ packages/frontend/src/app/modules/usager-profil/components/profil-historique/profil-historique.component.spec.ts
@@ -1,16 +1,49 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommonModule, APP_BASE_HREF } from "@angular/common";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { RouterTestingModule } from "@angular/router/testing";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { MatomoModule, MatomoInjector, MatomoTracker } from "ngx-matomo";
+import { ToastrModule } from "ngx-toastr";
 
-import { ProfilHistoriqueComponent } from './profil-historique.component';
+import { ProfilHistoriqueComponent } from "./profil-historique.component";
 
-describe('ProfilHistoriqueComponent', () => {
+describe("ProfilHistoriqueComponent", () => {
   let component: ProfilHistoriqueComponent;
   let fixture: ComponentFixture<ProfilHistoriqueComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProfilHistoriqueComponent ]
-    })
-    .compileComponents();
+      declarations: [ProfilHistoriqueComponent],
+      imports: [
+        NgbModule,
+        MatomoModule,
+        CommonModule,
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientTestingModule,
+        ToastrModule.forRoot(),
+        RouterTestingModule,
+      ],
+      providers: [
+        {
+          provide: MatomoInjector,
+          useValue: {
+            init: jest.fn(),
+          },
+        },
+        {
+          provide: MatomoTracker,
+          useValue: {
+            setUserId: jest.fn(),
+          },
+        },
+        { provide: APP_BASE_HREF, useValue: "/" },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +52,7 @@ describe('ProfilHistoriqueComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
