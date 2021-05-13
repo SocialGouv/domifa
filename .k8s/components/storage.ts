67ffe378de47b4dd1c5bdb4c7ab43d3cce6c209b
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-05-13 15:13:37 +0200

    fix: Upgrade k8s dependencies.
    fix: Update sealed secrets
    
    fix: Update preprod DB password.
    
    fix: Update preprod DB password.
    
    Add dummy env vars for k8s.
    
    fix: Use fakepreprod3 as preprod database.
    
    fix: Add production db secrets.
    
    fix: Ingress for production env.
    
    fix: Add volumes.
    
    fix: Add temp volumes to review branches.
    
    Restore db seeds on alpha branch.
    
    Add configmaps and secrets.
    
    chore: Run backend tests on Github actions.
    
    Add backend .env file.
    
    Add backend ci env file.
    
    Wait between docker db setup and tests run.
    
    Run tests sequentially.
    
    Add docker compose for tests.
    
    Set POSTGRES_HOST.
    
    Add POSTGRES_HOST.
    
    Add POSTGRES_DB.
    
    Add container debug.
    
    Change postgres port.
    
    Fix pg_isready.
    
    Remove debug.
    
    Name job steps.
    
    Add front-end tests.
    
    Add e2e tests.
    
    Wording.
    
    Add build and register jobs.
    
    Add backend image build and register.
    
    Add extra_args.
    
    Add backend ci Dockerfile.
    
    Use --dockerfile for Kaniko.
    
    Add rep checkout.
    
    Change backend Dockerfile path.
    
    Use salt-labs/action-kaniko.
    
    Use SOCIALGROOVYBOT_GHCR_PAT.
    
    Push to ghcr via aevea/action-kaniko.
    
    Various fix.

---
+++ .k8s/components/storage.ts
@@ -0,0 +1,22 @@
+import env from "@kosko/env";
+import type { SealedSecret } from "@kubernetes-models/sealed-secrets/bitnami.com/v1alpha1/SealedSecret";
+import { create } from "@socialgouv/kosko-charts/components/azure-pg";
+import gitlab from "@socialgouv/kosko-charts/environments/gitlab";
+import { loadYaml } from "@socialgouv/kosko-charts/utils/getEnvironmentComponent";
+import { updateMetadata } from "@socialgouv/kosko-charts/utils/updateMetadata";
+
+export default (): { kind: string }[] => {
+  const secret = loadYaml<SealedSecret>(env, `azure-storage.sealed-secret.yaml`);
+  if (!secret) {
+    return [];
+  }
+
+  const envParams = gitlab(process.env);
+  // add gitlab annotations
+  updateMetadata(secret, {
+    annotations: envParams.annotations ?? {},
+    labels: envParams.labels ?? {},
+    namespace: envParams.namespace,
+  });
+  return [secret];
+};
---
+++ _scripts/docker/postgres-restore-test-dump.docker-entrypoint.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+POSTGRES_HOST=postgres /app/_scripts/db/restore-postgres.sh --db=test
