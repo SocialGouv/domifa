5fb7c86cd5eb8ba61ae06075d6a7daa0e6e79d6b
toub <n.toublanc@gmail.com>
2021-05-19 09:44:28 +0200

    fix(TRI): tri liste des domiciliés ne fonctionne plus #1375 - customRefs

--- packages/frontend/src/app/modules/usagers/components/manage/usager-filter/services/usagersSorter.service.ts
+++ packages/frontend/src/app/modules/usagers/components/manage/usager-filter/services/usagersSorter.service.ts
@@ -13,90 +13,146 @@ function sortBy(
   if (!sortKey) {
     return usagers;
   }
-  return dataSorter.sortMultiple(usagers, {
-    getSortAttributes: (usager) => {
-      const sortAttributes: SortableAttribute[] = [];
 
-      if (sortKey === "RADIE" || sortKey === "REFUS") {
-        sortAttributes.push(
-          {
-            value: usager.decision?.dateFin,
-            asc: sortValue !== "descending",
-          },
-          {
-            value: usager.nom?.toLowerCase(),
-          },
-          {
-            value: usager.prenom?.toLowerCase(),
-          }
-        );
-      } else if (sortKey === "INSTRUCTION" || sortKey === "ATTENTE_DECISION") {
-        sortAttributes.push(
-          {
-            value: usager.decision?.dateDecision,
-            asc: sortValue !== "descending",
-          },
-          {
-            value: usager.nom?.toLowerCase(),
-          },
-          {
-            value: usager.prenom?.toLowerCase(),
-          }
-        );
-      } else if (sortKey === "VALIDE" || sortKey === "TOUS") {
-        sortAttributes.push(
-          {
-            value: usager.decision?.dateFin,
-            asc: sortValue !== "descending",
-          },
-          {
-            value: usager.nom?.toLowerCase(),
-          },
-          {
-            value: usager.prenom?.toLowerCase(),
-          }
-        );
-      } else if (sortKey === "PASSAGE") {
-        sortAttributes.push(
-          {
-            value: usager.lastInteraction?.dateInteraction,
-            asc: sortValue !== "descending",
-          },
-          {
-            value: usager.nom?.toLowerCase(),
-          },
-          {
-            value: usager.prenom?.toLowerCase(),
-          }
-        );
-      } else if (sortKey === "NAME") {
-        sortAttributes.push(
-          {
-            value: usager.nom?.toLowerCase(),
-            asc: sortValue !== "descending",
-          },
-          {
-            value: usager.prenom?.toLowerCase(),
-          }
-        );
-      } else if (sortKey === "ID") {
-        sortAttributes.push(
-          {
-            value: parseAsNumberOrString(usager.customRef),
-            asc: sortValue !== "descending",
-          },
-          {
-            value: usager.nom?.toLowerCase(),
-          },
-          {
-            value: usager.prenom?.toLowerCase(),
-          }
-        );
-      }
-      return sortAttributes;
-    },
-  });
+  const asc = sortValue !== "descending";
+
+  if (sortKey === "ID") {
+    return sortUsagersByCustomRef(usagers, asc);
+  } else {
+    return dataSorter.sortMultiple(usagers, {
+      getSortAttributes: (usager) => {
+        const sortAttributes: SortableAttribute[] = [];
+
+        if (sortKey === "RADIE" || sortKey === "REFUS") {
+          sortAttributes.push(
+            {
+              value: usager.decision?.dateFin,
+              asc,
+            },
+            {
+              value: usager.nom?.toLowerCase(),
+            },
+            {
+              value: usager.prenom?.toLowerCase(),
+            }
+          );
+        } else if (
+          sortKey === "INSTRUCTION" ||
+          sortKey === "ATTENTE_DECISION"
+        ) {
+          sortAttributes.push(
+            {
+              value: usager.decision?.dateDecision,
+              asc,
+            },
+            {
+              value: usager.nom?.toLowerCase(),
+            },
+            {
+              value: usager.prenom?.toLowerCase(),
+            }
+          );
+        } else if (sortKey === "VALIDE" || sortKey === "TOUS") {
+          sortAttributes.push(
+            {
+              value: usager.decision?.dateFin,
+              asc,
+            },
+            {
+              value: usager.nom?.toLowerCase(),
+            },
+            {
+              value: usager.prenom?.toLowerCase(),
+            }
+          );
+        } else if (sortKey === "PASSAGE") {
+          sortAttributes.push(
+            {
+              value: usager.lastInteraction?.dateInteraction,
+              asc,
+            },
+            {
+              value: usager.nom?.toLowerCase(),
+            },
+            {
+              value: usager.prenom?.toLowerCase(),
+            }
+          );
+        } else if (sortKey === "NAME") {
+          sortAttributes.push(
+            {
+              value: usager.nom?.toLowerCase(),
+              asc,
+            },
+            {
+              value: usager.prenom?.toLowerCase(),
+            }
+          );
+        }
+        return sortAttributes;
+      },
+    });
+  }
 }
+function sortUsagersByCustomRef(usagers: UsagerLight[], asc: boolean) {
+  // extract usagers with 'integer' or string' customRef format
+  let { usagersWithCustomRefInteger, usagersWithCustomRefString } =
+    usagers.reduce(
+      (acc, usager) => {
+        if (/^\d+$/.test(usager.customRef.trim())) {
+          acc.usagersWithCustomRefInteger.push(usager);
+        } else {
+          acc.usagersWithCustomRefString.push(usager);
+        }
+        return acc;
+      },
+      { usagersWithCustomRefInteger: [], usagersWithCustomRefString: [] } as {
+        usagersWithCustomRefInteger: UsagerLight[];
+        usagersWithCustomRefString: UsagerLight[];
+      }
+    );
+  // sort usagers with 'integer' customRef format
+  usagersWithCustomRefInteger = dataSorter.sortMultiple(
+    usagersWithCustomRefInteger,
+    {
+      getSortAttributes: (usager) => [
+        {
+          value: parseAsNumberOrString(usager.customRef?.trim()),
+          asc,
+        },
+        {
+          value: usager.nom?.toLowerCase(),
+        },
+        {
+          value: usager.prenom?.toLowerCase(),
+        },
+      ],
+    }
+  );
+  // sort usagers with string' customRef format
+  usagersWithCustomRefString = dataSorter.sortMultiple(
+    usagersWithCustomRefString,
+    {
+      getSortAttributes: (usager) => [
+        {
+          value: usager.customRef?.trim().toLowerCase(),
+          asc,
+        },
+        {
+          value: usager.nom?.trim().toLowerCase(),
+        },
+        {
+          value: usager.prenom?.trim().toLowerCase(),
+        },
+      ],
+    }
+  );
+  // is ASC: integer first
+  return asc
+    ? usagersWithCustomRefInteger.concat(usagersWithCustomRefString)
+    : usagersWithCustomRefString.concat(usagersWithCustomRefInteger);
+}
+
 function parseAsNumberOrString(customRef: string): any {
   if (/^\d+$/.test(customRef)) {
     const customRefAsNumber = parseInt(customRef, 10);
