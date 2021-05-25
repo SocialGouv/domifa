22d25b6e73568fb949c29feeea51540230cdaa6c
toub <n.toublanc@gmail.com>
2021-05-25 13:11:37 +0200

    feat: add tailwind jit

---
+++ packages/frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./src/app/**/*.{ts,html}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
