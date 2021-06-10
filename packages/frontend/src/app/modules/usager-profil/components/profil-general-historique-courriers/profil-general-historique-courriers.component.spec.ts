2e46108d127ecc0658a998e9a30dad2b3fdf9ed6
Yassine R <riffi.yassine@gmail.com>
2021-06-10 23:04:25 +0200

    fix: unit tests

--- packages/frontend/src/app/modules/usager-profil/components/profil-general-historique-courriers/profil-general-historique-courriers.component.spec.ts
+++ packages/frontend/src/app/modules/usager-profil/components/profil-general-historique-courriers/profil-general-historique-courriers.component.spec.ts
@@ -1,25 +1,61 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { usagerValideMock } from "./../../../../../_common/mocks/usager.mock";
+import { APP_BASE_HREF } from "@angular/common";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterTestingModule } from "@angular/router/testing";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { MatomoModule, MatomoInjector, MatomoTracker } from "ngx-matomo";
+import { ToastrModule } from "ngx-toastr";
+import { UsagerFormModel } from "../../../usagers/components/form/UsagerFormModel";
 
-import { ProfilGeneralHistoriqueCourriersComponent } from './profil-general-historique-courriers.component';
+import { ProfilGeneralHistoriqueCourriersComponent } from "./profil-general-historique-courriers.component";
 
-describe('ProfilGeneralHistoriqueCourriersComponent', () => {
+describe("ProfilGeneralHistoriqueCourriersComponent", () => {
   let component: ProfilGeneralHistoriqueCourriersComponent;
   let fixture: ComponentFixture<ProfilGeneralHistoriqueCourriersComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProfilGeneralHistoriqueCourriersComponent ]
-    })
-    .compileComponents();
+      declarations: [ProfilGeneralHistoriqueCourriersComponent],
+      imports: [
+        NgbModule,
+        MatomoModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        ToastrModule.forRoot(),
+        RouterTestingModule,
+      ],
+      providers: [
+        {
+          provide: MatomoInjector,
+          useValue: {
+            init: jest.fn(),
+          },
+        },
+        {
+          provide: MatomoTracker,
+          useValue: {
+            setUserId: jest.fn(),
+          },
+        },
+        { provide: APP_BASE_HREF, useValue: "/" },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ProfilGeneralHistoriqueCourriersComponent);
+    fixture = TestBed.createComponent(
+      ProfilGeneralHistoriqueCourriersComponent
+    );
     component = fixture.componentInstance;
+    component.usager = new UsagerFormModel(usagerValideMock);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
