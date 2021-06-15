661adcee04e72dc8cfc8ff528b976c52a04ece6b
Yassine R <riffi.yassine@gmail.com>
2021-06-15 18:13:32 +0200

    fix: refacto de fichiers de labels

--- packages/backend/src/usagers/services/generateMotifLabel.service.ts
+++ packages/backend/src/usagers/services/generateMotifLabel.service.ts
@@ -5,24 +5,23 @@ import {
 } from "../../_common/labels";
 
 export const generateMotifLabel = (usager: UsagerLight): string => {
+  let motif = "" as any;
   if (
     usager.decision.statut === "REFUS" ||
     usager.decision.statut === "RADIE"
   ) {
     if (usager.decision.motif === "AUTRE") {
-      usager.decision.motif =
+      motif =
         usager.decision.motifDetails !== ""
           ? "Autre motif" + usager.decision.motifDetails
           : ("Autre motif non précisé" as any);
     } else {
-      usager.decision.motif =
+      motif =
         usager.decision.statut === "REFUS"
           ? MOTIFS_REFUS_LABELS[usager.decision.motif]
           : (MOTIFS_RADIATION_LABELS[usager.decision.motif] as any);
     }
-  } else {
-    usager.decision.motif = "" as any;
   }
 
-  return usager.decision.motif;
+  return motif;
 };
