3c2d3eb862322168e44e1ef8790bd92672ece1d1
Yassine R <riffi.yassine@gmail.com>
2021-07-26 11:34:11 +0200

    fix(interactions): Correction de la date de dernier passage

---
+++ packages/frontend/src/_common/model/usager/constants/ENTRETIEN_SECTIONS.const.ts
@@ -0,0 +1,8 @@
+export const ENTRETIEN_LIEN_COMMUNE = {
+  RESIDENTIEL: "Résidentiel",
+  PARENTAL: "Parental",
+  FAMILIAL: "Familial",
+  PROFESSIONNEL: "Professionnel",
+  SOCIAL: "Social",
+  AUTRE: "Autre",
+};
