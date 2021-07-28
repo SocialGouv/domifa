f9c5eeb3adf263454ec197353a77d747fe51bbbe
Yassine R <riffi.yassine@gmail.com>
2021-07-28 23:43:29 +0200

    feat(lien-commune): migration finale

--- packages/backend/src/database/services/usager/usagerHistoryRepository.service.ts
+++ packages/backend/src/database/services/usager/usagerHistoryRepository.service.ts
@@ -2,8 +2,9 @@ import { UsagerHistory } from "../../../_common/model";
 import { UsagerHistoryTable } from "../../entities/usager/UsagerHistoryTable.typeorm";
 import { pgRepository } from "../_postgres/pgRepository.service";
 
-const baseRepository =
-  pgRepository.get<UsagerHistoryTable, UsagerHistory>(UsagerHistoryTable);
+const baseRepository = pgRepository.get<UsagerHistoryTable, UsagerHistory>(
+  UsagerHistoryTable
+);
 
 export const usagerHistoryRepository = {
   ...baseRepository,
