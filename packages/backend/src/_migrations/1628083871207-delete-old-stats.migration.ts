6300c98bc76002848dadf5427fd753c8178eff24
Yassine R <riffi.yassine@gmail.com>
2021-08-04 18:57:21 +0200

    fix: drop structues-stats

--- packages/backend/src/_migrations/1628083871207-delete-old-stats.migration.ts
+++ packages/backend/src/_migrations/1628083871207-delete-old-stats.migration.ts
@@ -4,11 +4,8 @@ import { appLogger } from "../util";
 export class deleteStatsMigration1628083871207 implements MigrationInterface {
   name = "deleteStatsMigration1628083871207";
   public async up(queryRunner: QueryRunner): Promise<void> {
-    appLogger.warn("[MIGRATIO] Delete old stats");
-    await queryRunner.query(
-      `ALTER TABLE "structure_stats" DROP CONSTRAINT "FK_32881a91eaf51f28d3f9cf09589"`
-    );
-    await queryRunner.query(`DROP TABLE "structure_stats"`);
+    appLogger.warn("[MIGRATION] Delete old stats");
+    await queryRunner.query(`DROP  TABLE IF EXISTS "structure_stats"`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
