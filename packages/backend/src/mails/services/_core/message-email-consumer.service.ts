14371641423011dcb3e83c7e9745e1aec7fb20e5
Yassine R <riffi.yassine@gmail.com>
2021-08-21 00:01:27 +0200

    fix: correction de tests

--- packages/backend/src/mails/services/_core/message-email-consumer.service.ts
+++ packages/backend/src/mails/services/_core/message-email-consumer.service.ts
@@ -20,15 +20,14 @@ import moment = require("moment");
 @Injectable()
 export class MessageEmailConsummer {
   constructor() {
-    // TODO: à fixer avant mise en production
-    // messageEmailConsummerTrigger.trigger$
-    //   .pipe(
-    //     debounceTime(1000),
-    //     concatMap((trigger: MonitoringBatchProcessTrigger) =>
-    //       from(this.consumeEmails(trigger))
-    //     )
-    //   )
-    //   .subscribe();
+    messageEmailConsummerTrigger.trigger$
+      .pipe(
+        debounceTime(1000),
+        concatMap((trigger: MonitoringBatchProcessTrigger) =>
+          from(this.consumeEmails(trigger))
+        )
+      )
+      .subscribe();
   }
 
   @Cron(domifaConfig().cron.emailConsumer.crontime)
