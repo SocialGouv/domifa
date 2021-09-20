08de58aa8625244af2313a3a483942609a0370f7
Yassine R <riffi.yassine@gmail.com>
2021-09-20 14:23:29 +0200

    feat(project): lancement du portail des usagers feat(design): mise à jour du Favicon

---
+++ packages/portail-usagers/jest.config.js
@@ -0,0 +1,28 @@
+// require("jest-preset-angular/ngcc-jest-processor");
+
+module.exports = {
+  preset: "jest-preset-angular",
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/src/tsconfig.spec.json",
+      stringifyContentPathRegex: "\\.html$",
+    },
+  },
+  moduleFileExtensions: ["ts", "html", "js", "tsx", "json"],
+  moduleNameMapper: {
+    "^src/(.*)$": "<rootDir>/src/$1",
+    "^app/(.*)$": "<rootDir>/src/app/$1",
+    "^assets/(.*)$": "<rootDir>/src/assets/$1",
+    "^environments/(.*)$": "<rootDir>/src/environments/$1",
+  },
+  transform: {
+    "^.+\\.(js|ts|tsx|html)$": "jest-preset-angular",
+  },
+  transformIgnorePatterns: ["node_modules/(?!@ngrx)"],
+  setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
+  snapshotSerializers: [
+    "jest-preset-angular/build/serializers/no-ng-attributes",
+    "jest-preset-angular/build/serializers/ng-snapshot",
+    "jest-preset-angular/build/serializers/html-comment",
+  ],
+};
