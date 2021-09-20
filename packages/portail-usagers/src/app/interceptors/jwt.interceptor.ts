33f9d5e3116b06beb7db7c99da0477fe42688a01
Yassine R <riffi.yassine@gmail.com>
2021-09-20 16:51:39 +0200

    fix: build & tests

--- packages/portail-usagers/src/app/interceptors/jwt.interceptor.ts
+++ packages/portail-usagers/src/app/interceptors/jwt.interceptor.ts
@@ -16,14 +16,14 @@ export class JwtInterceptor implements HttpInterceptor {
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    const currentUser = this.authService.currentUserValue;
-    if (currentUser && currentUser.access_token) {
-      request = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${currentUser.access_token}`,
-        },
-      });
-    }
+    // const currentUser = this.authService.currentUserValue;
+    // if (currentUser && currentUser.access_token) {
+    //   request = request.clone({
+    //     setHeaders: {
+    //       Authorization: `Bearer ${currentUser.access_token}`,
+    //     },
+    //   });
+    // }
 
     return next.handle(request);
   }
