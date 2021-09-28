234765605915a07a16dd9cc63432f1aa0f77fe9c
Yassine R <riffi.yassine@gmail.com>
2021-09-28 17:33:41 +0200

    fix: tests unitaires sur le login

--- packages/frontend/src/app/modules/users/components/edit-user/edit-user.component.spec.ts
+++ packages/frontend/src/app/modules/users/components/edit-user/edit-user.component.spec.ts
@@ -15,21 +15,23 @@ describe("EditUserComponent", () => {
   let component: EditUserComponent;
   let fixture: ComponentFixture<EditUserComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [EditUserComponent],
-      imports: [
-        NgbModule,
-        ReactiveFormsModule,
-        FormsModule,
-        ToastrModule.forRoot(),
-        HttpClientTestingModule,
-        RouterTestingModule,
-      ],
-      providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [EditUserComponent],
+        imports: [
+          NgbModule,
+          ReactiveFormsModule,
+          FormsModule,
+          ToastrModule.forRoot(),
+          HttpClientTestingModule,
+          RouterTestingModule,
+        ],
+        providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EditUserComponent);
@@ -39,5 +41,34 @@ describe("EditUserComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("Password Form", () => {
+    component.initPasswordForm();
+
+    expect(component.editPassword).toBe(true);
+
+    component.passwordForm.controls.oldPassword.setValue("");
+    expect(component.passwordForm.valid).toBeFalsy();
+
+    component.passwordForm.controls.oldPassword.setValue("3Mxx!8ZnhST_zaa");
+
+    component.passwordForm.controls.password.setValue("4uxxxtmS8TB!D983E");
+    component.passwordForm.controls.confirmPassword.setValue(
+      "4uxxxtmS8TB!D983E"
+    );
+    expect(component.passwordForm.valid).toBeTruthy();
+
+    expect(component.hideOldPassword).toBe(true);
+    expect(component.hidePassword).toBe(true);
+    expect(component.hideConfirmPassword).toBe(true);
+
+    component.togglePassword();
+    component.togglePasswordConfirmation();
+    component.toggleOldPassword();
+
+    expect(component.hideOldPassword).toBe(false);
+    expect(component.hidePassword).toBe(false);
+    expect(component.hideConfirmPassword).toBe(false);
   });
 });
--- packages/frontend/src/app/modules/users/components/login/login.component.spec.ts
+++ packages/frontend/src/app/modules/users/components/login/login.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 
 import { APP_BASE_HREF } from "@angular/common";
-import { HttpClientModule } from "@angular/common/http";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
@@ -15,21 +14,23 @@ describe("LoginComponent", () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [LoginComponent],
-      imports: [
-        NgbModule,
-        ReactiveFormsModule,
-        FormsModule,
-        HttpClientTestingModule,
-        RouterTestingModule,
-        ToastrModule.forRoot({}),
-      ],
-      providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [LoginComponent],
+        imports: [
+          NgbModule,
+          ReactiveFormsModule,
+          FormsModule,
+          HttpClientTestingModule,
+          RouterTestingModule,
+          ToastrModule.forRoot({}),
+        ],
+        providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
@@ -39,5 +40,18 @@ describe("LoginComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("Login form", () => {
+    expect(component.loginForm.valid).toBeFalsy();
+
+    component.loginForm.controls.email.setValue("MyName");
+    component.loginForm.controls.password.setValue("123456789");
+    expect(component.loginForm.valid).toBeFalsy();
+
+    component.loginForm.controls.email.setValue("mon@mail.fr");
+    expect(component.loginForm.valid).toBeTruthy();
+
+    expect(component.returnUrl).toBe("/");
   });
 });
