528a0f3c5e99eca639f460d7dda8279aba5e8807
Yassine R <riffi.yassine@gmail.com>
2021-10-26 00:03:58 +0200

    fix: 3eme essai

--- _scripts/db/restore-data-only-docker.sh
+++ _scripts/db/restore-data-only-docker.sh
@@ -37,7 +37,7 @@ case $i in
 esac
 done
 
-if [ -z "${TARGET_DB_ENV}" ] 
+if [ -z "${TARGET_DB_ENV}" ]
 then
   echo ""
   echo "----------------------------------------------------------------------------------------------"
@@ -46,7 +46,7 @@ then
   exit 1
 fi
 
-if [ -z "${DUMP_ENV}" ] 
+if [ -z "${DUMP_ENV}" ]
 then
   DUMP_ENV="${TARGET_DB_ENV}"
 fi
