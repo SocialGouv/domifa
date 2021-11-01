2b201fac8893af42227f2d8a3caededaa1a2bad1
Yassine R <riffi.yassine@gmail.com>
2021-11-01 17:37:36 +0100

    fix: mise à jour des tests

--- packages/frontend/src/app/modules/usager-dossier/components/step-footer/step-footer.component.spec.ts
+++ packages/frontend/src/app/modules/usager-dossier/components/step-footer/step-footer.component.spec.ts
@@ -1,16 +1,51 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommonModule, APP_BASE_HREF } from "@angular/common";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { RouterTestingModule } from "@angular/router/testing";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { MatomoModule, MatomoInjector, MatomoTracker } from "ngx-matomo";
+import { ToastrModule } from "ngx-toastr";
+import { SharedModule } from "../../../shared/shared.module";
 
-import { StepFooterComponent } from './step-footer.component';
+import { StepFooterComponent } from "./step-footer.component";
 
-describe('StepFooterComponent', () => {
+describe("StepFooterComponent", () => {
   let component: StepFooterComponent;
   let fixture: ComponentFixture<StepFooterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ StepFooterComponent ]
-    })
-    .compileComponents();
+      declarations: [StepFooterComponent],
+      imports: [
+        NgbModule,
+        MatomoModule,
+        CommonModule,
+        ReactiveFormsModule,
+        FormsModule,
+        SharedModule,
+        HttpClientTestingModule,
+        ToastrModule.forRoot(),
+        RouterTestingModule,
+      ],
+      providers: [
+        {
+          provide: MatomoInjector,
+          useValue: {
+            init: jest.fn(),
+          },
+        },
+        {
+          provide: MatomoTracker,
+          useValue: {
+            setUserId: jest.fn(),
+          },
+        },
+        { provide: APP_BASE_HREF, useValue: "/" },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +54,7 @@ describe('StepFooterComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
