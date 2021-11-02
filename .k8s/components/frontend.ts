747604565c03dce6aa15e460458b69ffa23d4fb4
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-11-02 17:52:07 +0100

    chore(data): Restore database according current branch name

--- .k8s/components/frontend.ts
+++ .k8s/components/frontend.ts
@@ -14,7 +14,7 @@ export const getManifests = async () => {
   const name = "frontend";
   const subdomain = "domifa";
   const ciEnv = environments(process.env);
-  const version = ciEnv.tag || `sha-${ciEnv.sha}`;
+  const version = ciEnv.isPreProduction ? `preprod-${ciEnv.sha}` : ciEnv.tag || `sha-${ciEnv.sha}`;
 
   const podProbes = ["livenessProbe", "readinessProbe", "startupProbe"].reduce(
     (probes, probe) => ({
