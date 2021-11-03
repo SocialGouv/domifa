b4fcf9c342a62a6e1c3381e9cbf4be60f528b8b7
toub <n.toublanc@gmail.com>
2021-11-03 10:34:27 +0100

    perf(pg): analyse performances pg #1776

--- packages/backend/src/_migrations/1635801057529-set-typedom-default-migration.ts
+++ packages/backend/src/_migrations/1635801057529-set-typedom-default-migration.ts
@@ -1,9 +1,11 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
+import { appLogger } from "../util";
 
 export class autoMigration1635801057529 implements MigrationInterface {
   name = "setTypeDomDefault1635801057529";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    appLogger.warn(`[${this.name}] MIGRATION UP...`);
     await queryRunner.query(
       `ALTER TABLE "usager" ALTER COLUMN "typeDom" SET DEFAULT 'PREMIERE_DOM'`
     );
