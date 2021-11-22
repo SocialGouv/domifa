8d33f8ef0ffb2e5b9046b13f609d169b3f21ff3e
Yassine R <riffi.yassine@gmail.com>
2021-11-22 21:04:43 +0100

    fix: tests unitaires

--- packages/backend/src/usagers/controllers/usager-structure-docs.controller.security-tests.ts
+++ packages/backend/src/usagers/controllers/usager-structure-docs.controller.security-tests.ts
@@ -4,6 +4,7 @@ import {
   AppTestHttpClientSecurityTestDef,
   expectedResponseStatusBuilder,
 } from "../../_tests";
+import { v4 as uuidv4 } from "uuid";
 
 ////////////////// IMPORTANT //////////////////
 //
@@ -12,14 +13,14 @@ import {
 //
 
 const CONTROLLER = "UsagersStructureDocsController";
-
+const FAKE_UUID = uuidv4();
 export const UsagersStructureDocsControllerSecurityTests: AppTestHttpClientSecurityTestDef[] =
   [
     {
       label: `${CONTROLLER}.getStructureCustomDoc`,
       query: async (context: AppTestContext) => ({
         response: await AppTestHttpClient.get(
-          "/usagers-structure-docs/structure/1/xxx",
+          "/usagers-structure-docs/structure/1/" + FAKE_UUID,
           {
             context,
           }
