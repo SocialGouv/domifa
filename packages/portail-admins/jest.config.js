addf43ad47d247222f716fe5070c57afbac2cf1c
Yassine R <riffi.yassine@gmail.com>
2021-11-30 00:01:43 +0100

    fix: correction suite à la migration v13

--- packages/portail-admins/jest.config.js
+++ packages/portail-admins/jest.config.js
@@ -3,26 +3,16 @@ require("jest-preset-angular/ngcc-jest-processor");
 module.exports = {
   globals: {
     "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.spec.json",
       stringifyContentPathRegex: "\\.html$",
-      tsconfig: "<rootDir>/src/tsconfig.spec.json",
     },
   },
-  moduleFileExtensions: ["ts", "html", "js", "tsx", "json"],
+  preset: "jest-preset-angular",
   moduleNameMapper: {
+    "^src/(.*)$": "<rootDir>/src/$1",
     "^app/(.*)$": "<rootDir>/src/app/$1",
     "^assets/(.*)$": "<rootDir>/src/assets/$1",
     "^environments/(.*)$": "<rootDir>/src/environments/$1",
-    "^src/(.*)$": "<rootDir>/src/$1",
   },
-  preset: "jest-preset-angular",
-  setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
-  snapshotSerializers: [
-    "jest-preset-angular/build/serializers/no-ng-attributes",
-    "jest-preset-angular/build/serializers/ng-snapshot",
-    "jest-preset-angular/build/serializers/html-comment",
-  ],
-  transform: {
-    "^.+\\.(js|ts|tsx|html)$": "jest-preset-angular",
-  },
-  transformIgnorePatterns: ["node_modules/(?!@ngrx)"],
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
 };
