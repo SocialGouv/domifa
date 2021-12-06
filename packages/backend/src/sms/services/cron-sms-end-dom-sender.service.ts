4b6dcbea9983710f8babb1fc4c006963eee9a241
Olivier Leung <pibouu@gmail.com>
2021-12-06 14:47:05 +0100

    feat: add new notif end dom

--- packages/backend/src/sms/services/cron-sms-end-dom-sender.service.ts
+++ packages/backend/src/sms/services/cron-sms-end-dom-sender.service.ts
@@ -7,37 +7,34 @@ import {
   MonitoringBatchProcessTrigger,
 } from "../../database";
 import { messageSmsRepository } from "../../database/services/message-sms";
-import { appLogger } from "../../util";
 import { MessageSmsSenderService } from "./message-sms-sender.service";
+import { appLogger } from "../../util";
 
 @Injectable()
-export class CronSmsInteractionSenderService {
+export class CronSmsEndDomSenderService {
   constructor(private messageSmsSenderService: MessageSmsSenderService) {}
 
-  @Cron(domifaConfig().cron.smsConsumer.crontime)
-  protected async sendSmsInteractionCron() {
+  @Cron(domifaConfig().cron.smsConsumer.sendEndDomCronTim)
+  public async sendSmsUsagerEndDom(trigger: MonitoringBatchProcessTrigger) {
     if (!domifaConfig().cron.enable || !domifaConfig().sms.enabled) {
       // Désactiver tous les SMS en attente
       appLogger.warn(`[CronSms] Disable all SMS to Send`);
       return this.messageSmsSenderService.disableAllSmsToSend();
     }
-    await this.sendSmsInteraction("cron");
-  }
 
-  public async sendSmsInteraction(trigger: MonitoringBatchProcessTrigger) {
     await monitoringBatchProcessSimpleCountRunner.monitorProcess(
       {
         processId: "sms-messages-consumer",
         trigger,
       },
       async ({ monitorTotal, monitorSuccess, monitorError }) => {
-        const messageSmsList =
-          await messageSmsRepository.findInteractionSmsToSend();
+        const messageSmsList = await messageSmsRepository.findSmsEndDomToSend();
 
         monitorTotal(messageSmsList.length);
 
         for (const messageSms of messageSmsList) {
           try {
+            console.log("sending sms ->");
             await this.messageSmsSenderService.sendSms(messageSms);
             monitorSuccess();
           } catch (err) {
