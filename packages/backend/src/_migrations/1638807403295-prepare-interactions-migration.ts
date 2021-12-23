a247832d5b5ac3718c69e22a96beca114b43708c
Yassine R <riffi.yassine@gmail.com>
2021-12-23 18:30:23 +0100

    fix: mise à jour de la DB pour les tests

--- packages/backend/src/_migrations/1638807403295-prepare-interactions-migration.ts
+++ packages/backend/src/_migrations/1638807403295-prepare-interactions-migration.ts
@@ -5,33 +5,21 @@ import { appLogger } from "../util";
 export class manualMigration1638807403295 implements MigrationInterface {
   name = "prepareInteractionMigration1635801057529";
   public async up(queryRunner: QueryRunner): Promise<void> {
-    if (
-      domifaConfig().envId === "prod" ||
-      domifaConfig().envId === "preprod" ||
-      domifaConfig().envId === "test"
-    ) {
-      await queryRunner.query(
-        `ALTER TABLE "usager" ADD "interactionsMigrated" boolean NOT NULL DEFAULT false`
-      );
-      await queryRunner.query(
-        `ALTER TABLE "usager" ADD "interactionsDifference" boolean NOT NULL DEFAULT false`
-      );
-      await queryRunner.query(
-        `ALTER TABLE "interactions" ADD "interactionOutUUID" uuid`
-      );
-      await queryRunner.query(
-        `CREATE INDEX "IDX_495b59d0dd15e43b262f2da890" ON "interactions" ("interactionOutUUID") `
-      );
-      await queryRunner.query(
-        `ALTER TABLE "interactions" ADD CONSTRAINT "FK_495b59d0dd15e43b262f2da8907" FOREIGN KEY ("interactionOutUUID") REFERENCES "interactions"("uuid") ON DELETE NO ACTION ON UPDATE NO ACTION`
-      );
-    } else {
-      appLogger.debug(
-        "[DEBUG] Skipped because " +
-          domifaConfig().envId +
-          " is not prod or prperod"
-      );
-    }
+    await queryRunner.query(
+      `ALTER TABLE "usager" ADD "interactionsMigrated" boolean NOT NULL DEFAULT false`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "usager" ADD "interactionsDifference" boolean NOT NULL DEFAULT false`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "interactions" ADD "interactionOutUUID" uuid`
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_495b59d0dd15e43b262f2da890" ON "interactions" ("interactionOutUUID") `
+    );
+    await queryRunner.query(
+      `ALTER TABLE "interactions" ADD CONSTRAINT "FK_495b59d0dd15e43b262f2da8907" FOREIGN KEY ("interactionOutUUID") REFERENCES "interactions"("uuid") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
