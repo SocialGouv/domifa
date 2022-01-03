991a81bf4d71c4ca0dc53574631fcb85a9e08b5c
Olivier Leung <pibouu@gmail.com>
2022-01-03 13:00:52 +0100

    fix: migration bloquante

--- packages/backend/src/_migrations/1638807403295-prepare-interactions-migration.ts
+++ packages/backend/src/_migrations/1638807403295-prepare-interactions-migration.ts
@@ -6,12 +6,8 @@ export class manualMigration1638807403295 implements MigrationInterface {
   name = "prepareInteractionMigration1635801057529";
   public async up(queryRunner: QueryRunner): Promise<void> {
     const envId = domifaConfig().envId;
-    appLogger.debug(
-      "[MIGRATION] prepareInteractionMigration1635801057529 start"
-    );
-    if (envId === "prod" || envId === "preprod") {
-      appLogger.debug("[MIGRATION] Création de interactionsMigrated ");
 
+    if (envId === "prod" || envId === "preprod") {
       await queryRunner.query(
         `ALTER TABLE "usager" ADD "interactionsMigrated" boolean NOT NULL DEFAULT false`
       );
@@ -24,9 +20,11 @@ export class manualMigration1638807403295 implements MigrationInterface {
       await queryRunner.query(
         `ALTER TABLE "interactions" ADD CONSTRAINT "FK_495b59d0dd15e43b262f2da8907" FOREIGN KEY ("interactionOutUUID") REFERENCES "interactions"("uuid") ON DELETE NO ACTION ON UPDATE NO ACTION`
       );
+    } else {
+      appLogger.debug(
+        "[MIGRATION] Création des données esquivée, car pas dans un env PROD, PREPROD"
+      );
     }
-
-    appLogger.debug("[MIGRATION] Initialisation de interactionsMigrated");
     await queryRunner.query(
       `UPDATE "usager" SET "interactionsMigrated" = false`
     );
