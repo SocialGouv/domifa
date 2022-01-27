dfe379dbf3a801796f2fd55a246c0d2acbd90e97
Yassine R <riffi.yassine@gmail.com>
2022-01-27 01:50:13 +0100

    fix(stats): affichage des statistiques

--- packages/frontend/src/app/modules/stats/components/elements/stats-map/stats-map.component.ts
+++ packages/frontend/src/app/modules/stats/components/elements/stats-map/stats-map.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, OnInit } from "@angular/core";
+import { AfterContentChecked, Component, Input, OnInit } from "@angular/core";
 import {
   PublicStats,
   StatsByRegion,
@@ -18,15 +18,23 @@ import {
   templateUrl: "./stats-map.component.html",
   styleUrls: ["./stats-map.component.css"],
 })
-export class StatsMapComponent implements OnInit, AfterViewInit {
+export class StatsMapComponent implements OnInit, AfterContentChecked {
+  // Liste des régions
   public STATS_REGIONS_DOM_TOM = ["01", "02", "03", "04", "06"];
   public STATS_REGIONS_FOR_MAP = STATS_REGIONS_FOR_MAP;
+
+  // Labels des régions
   public regions: RegionsLabels = REGIONS_LABELS_MAP;
+
+  // Urls des régions
   public regionsUrls: RegionsLabels = REGIONS_SEO_URL_MAP;
 
+  // Région choisie
   public selectedRegion: string;
 
+  // Statistiques par region
   public statsRegionsValues: { [key: string]: number };
+
   @Input() public publicStats: PublicStats;
 
   public statsByRegion: StatsByRegion;
@@ -44,7 +52,7 @@ export class StatsMapComponent implements OnInit, AfterViewInit {
     }
   }
 
-  public ngAfterViewInit(): void {
+  public ngAfterContentChecked(): void {
     this.statsByRegion = this.publicStats.structuresCountByRegion;
 
     this.statsRegionsValues = Object.values(REGIONS_DEF).reduce(
