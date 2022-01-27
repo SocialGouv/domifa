3d6e3b723b14fcea92705b482a2b64fe97ef3525
Yassine R <riffi.yassine@gmail.com>
2022-01-27 01:50:13 +0100

    fix(stats): affichage des statistiques

--- packages/frontend/src/app/modules/stats/components/elements/stats-map/stats-map.component.spec.ts
+++ packages/frontend/src/app/modules/stats/components/elements/stats-map/stats-map.component.spec.ts
@@ -1,6 +1,11 @@
+import { APP_BASE_HREF } from "@angular/common";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterTestingModule } from "@angular/router/testing";
 import { DEFAULT_PUBLIC_STATS } from "../../../../../../_common/model/stats/DEFAULT_PUBLIC_STATS.const";
+import { SharedModule } from "../../../../shared/shared.module";
 
 import { StatsMapComponent } from "./stats-map.component";
 
@@ -11,6 +16,14 @@ describe("StatsMapComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [StatsMapComponent],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        BrowserAnimationsModule,
+        SharedModule,
+      ],
+      providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
+
       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
     }).compileComponents();
   });
@@ -19,7 +32,8 @@ describe("StatsMapComponent", () => {
     fixture = TestBed.createComponent(StatsMapComponent);
     component = fixture.componentInstance;
     component.publicStats = DEFAULT_PUBLIC_STATS;
-    component.statsByRegion = [];
+    component.statsRegionsValues = {};
+    component.selectedRegion = null;
 
     fixture.detectChanges();
   });
