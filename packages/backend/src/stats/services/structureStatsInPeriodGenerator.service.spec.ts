ca4caf30d86a2086fc5058a3a4d5b32824b70e3b
Yassine R <riffi.yassine@gmail.com>
2022-02-02 00:04:51 +0100

    fix: correction des tests

--- packages/backend/src/stats/services/structureStatsInPeriodGenerator.service.spec.ts
+++ packages/backend/src/stats/services/structureStatsInPeriodGenerator.service.spec.ts
@@ -54,9 +54,9 @@ describe("structureStatsInPeriodGenerator", () => {
       },
       interactions: {
         appel: 0,
-        colisIn: 7,
+        colisIn: 4,
         colisOut: 0,
-        courrierIn: 1,
+        courrierIn: 4,
         courrierOut: 0,
         npai: 0,
         recommandeIn: 3,
--- packages/backend/src/usagers/dto/index.ts
+++ packages/backend/src/usagers/dto/index.ts
@@ -2,11 +2,12 @@
 export * from "./create-note.dto";
 export * from "./CreateUsagerDto";
 export * from "./decision.dto";
-
 export * from "./entretien.dto";
-export * from "./PreferenceContact.dto";
+export * from "./PreferenceContactDto.dto";
 export * from "./procuration.dto";
 export * from "./rdv.dto";
+export * from "./search-usager.dto";
 export * from "./search.dto";
 export * from "./transfert.dto";
 export * from "./UpdatePortailUsagerOptionsDto";
+export * from "./UsagerAyantDroitDto";
