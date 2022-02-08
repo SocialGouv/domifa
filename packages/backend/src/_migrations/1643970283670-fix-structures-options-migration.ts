4c87686858a7a2e8dede79e49243c3a58f124cd8
Yassine R <riffi.yassine@gmail.com>
2022-02-08 21:35:56 +0100

    fix(mails): correction des entetes

--- packages/backend/src/_migrations/1643970283670-fix-structures-options-migration.ts
+++ packages/backend/src/_migrations/1643970283670-fix-structures-options-migration.ts
@@ -1,49 +1,33 @@
+import { appLogger } from "./../util/AppLogger.service";
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 import { structureRepository } from "../database";
-
-const emptyStructures = [
-  794, 793, 782, 777, 801, 809, 797, 812, 784, 789, 783, 802, 811, 780, 788,
-  799, 785, 790, 792, 798, 786, 781, 795, 779, 791, 796, 800, 804, 803, 808,
-  806, 805, 778, 787, 810, 807,
-];
 
 export class manualMigration1643970283670 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     const structuresWithEmptyOptions = await (
       await structureRepository.typeorm()
-    ).query(`
-            SELECT *
-            FROM "structure"
-            WHERE "options" is NULL
-        `);
+    )
+      .createQueryBuilder("structures")
+      .select()
+      .where(` "options" is NULL`)
+      .execute();
 
-    for (const structure of structuresWithEmptyOptions) {
-      await (
-        await structureRepository.typeorm()
-      ).update(
-        {
-          uuid: structure.uuid,
-        },
-        {
-          options: { numeroBoite: false },
-        }
-      );
-    }
+    appLogger.debug(
+      `[MIGRATION] ${structuresWithEmptyOptions.length} structures à mettre à jour`
+    );
+
+    const test = await (
+      await structureRepository.typeorm()
+    )
+      .createQueryBuilder("structures")
+      .update()
+      .set({ options: { numeroBoite: false } })
+      .where(` "options" is NULL`)
+      .execute();
+
+    console.log(test);
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    for (const id of emptyStructures) {
-      await (
-        await structureRepository.typeorm()
-      ).update(
-        {
-          id,
-        },
-        {
-          options: null,
-        }
-      );
-    }
-  }
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }
