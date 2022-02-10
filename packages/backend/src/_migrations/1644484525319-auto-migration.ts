6928d5f3acc64752a2606fb7fabbc80392e62be9
Yassine R <riffi.yassine@gmail.com>
2022-02-10 10:52:10 +0100

    fix: deploy

--- packages/backend/src/_migrations/1644484525319-auto-migration.ts
+++ packages/backend/src/_migrations/1644484525319-auto-migration.ts
@@ -1,12 +1,15 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
+import { domifaConfig } from "../config";
 
 export class autoMigration1644484525319 implements MigrationInterface {
   name = "autoMigration1644484525319";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "contact_support" RENAME COLUMN "attachement" TO "attachment"`
-    );
+    if (domifaConfig().envId === "prod" || domifaConfig().envId === "preprod") {
+      await queryRunner.query(
+        `ALTER TABLE "contact_support" RENAME COLUMN "attachement" TO "attachment"`
+      );
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
