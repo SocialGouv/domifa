007dba2fd28ee39f3b53c27bc6c69251806aad45
Yassine R <riffi.yassine@gmail.com>
2022-04-07 06:25:35 +0200

    test: Correction des tests frontend

--- packages/frontend/src/app/modules/usager-shared/components/delete-usager-menu/delete-usager-menu.component.spec.ts
+++ packages/frontend/src/app/modules/usager-shared/components/delete-usager-menu/delete-usager-menu.component.spec.ts
@@ -1,15 +1,17 @@
+import { USER_STRUCTURE_MOCK } from "./../../../../../_common/mocks/USER_STRUCTURE.mock";
+import { USAGER_ACTIF_MOCK } from "./../../../../../_common/mocks/USAGER_ACTIF.mock";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { NO_ERRORS_SCHEMA } from "@angular/core";
 
 import { DeleteUsagerMenuComponent } from "./delete-usager-menu.component";
 import { APP_BASE_HREF } from "@angular/common";
-import { MatomoInjector, MatomoTracker } from "ngx-matomo";
 
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { RouterTestingModule } from "@angular/router/testing";
+import { UsagerFormModel } from "../../interfaces";
 
 describe("DeleteUsagerMenuComponent", () => {
   let component: DeleteUsagerMenuComponent;
@@ -17,33 +19,15 @@ describe("DeleteUsagerMenuComponent", () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        NgbModule,
-        HttpClientTestingModule,
-
-        HttpClientTestingModule,
-      ],
+      imports: [RouterTestingModule, NgbModule, HttpClientTestingModule],
       schemas: [NO_ERRORS_SCHEMA],
-      providers: [
-        {
-          provide: MatomoInjector,
-          useValue: {
-            init: jest.fn(),
-          },
-        },
-        {
-          provide: MatomoTracker,
-          useValue: {
-            setUserId: jest.fn(),
-          },
-        },
-        { provide: APP_BASE_HREF, useValue: "/" },
-      ],
+      providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
       declarations: [DeleteUsagerMenuComponent],
     });
     fixture = TestBed.createComponent(DeleteUsagerMenuComponent);
     component = fixture.debugElement.componentInstance;
+    component.usager = new UsagerFormModel(USAGER_ACTIF_MOCK);
+    component.me = USER_STRUCTURE_MOCK;
     component.ngOnInit();
   });
 
