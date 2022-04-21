a7cca45e893430a1a59bcbadaac6823731d0915b
Yassine R <riffi.yassine@gmail.com>
2022-04-21 04:18:25 +0200

    fix: suppresion de données obsolètes

---
+++ packages/backend/src/_migrations/1650494327593-fix-premier-dom-migration.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+import { domifaConfig } from "../config";
+import { usagerRepository } from "../database";
+import { appLogger } from "../util";
+import { Usager } from "../_common/model";
+
+export class fixPremiereDomMigration1650494327593
+  implements MigrationInterface
+{
+  name = "fixPremiereDomMigration1650494327593";
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    if (
+      domifaConfig().envId === "prod" ||
+      domifaConfig().envId === "preprod" ||
+      domifaConfig().envId === "local"
+    ) {
+      appLogger.warn("[MIGRATION] Nettoyage de données obsolètes");
+      const usagers: Usager[] = await (
+        await usagerRepository.typeorm()
+      ).query(
+        `select * from usager u where decision->>'statut' = 'PREMIERE_DOM' OR decision->>'statut' = 'PREMIERE' OR decision->>'statut' = 'IMPORT'`
+      );
+
+      appLogger.warn(
+        `[MIGRATION] [FIX PREMIERE_DOM] ${usagers.length} dossiers à mettre à jour`
+      );
+      // Correction des datas obsolètes de l'historique des premieres dom
+      for (const usager of usagers) {
+        usager.decision.statut = "VALIDE";
+
+        await (
+          await usagerRepository.typeorm()
+        ).update(
+          {
+            uuid: usager.uuid,
+          },
+          {
+            decision: usager.decision,
+          }
+        );
+      }
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
