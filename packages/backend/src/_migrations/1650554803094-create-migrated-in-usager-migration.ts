7eb32d233ec8268e87687c63d82330c2b474ee38
Yassine R <riffi.yassine@gmail.com>
2022-04-28 01:10:44 +0200

    fix(dumps): correction des dumps

--- packages/backend/src/_migrations/1650554803094-create-migrated-in-usager-migration.ts
+++ packages/backend/src/_migrations/1650554803094-create-migrated-in-usager-migration.ts
@@ -22,9 +22,6 @@ export class createMigratedDataMigration1650554803094
       await queryRunner.query(
         `ALTER TABLE "usager" ALTER COLUMN "options" SET DEFAULT '{ "transfert":{ "actif":false, "nom":null, "adresse":null, "dateDebut":null, "dateFin":null }, "procurations":[], "npai":{ "actif":false, "dateDebut":null }, "portailUsagerEnabled":false }'`
       );
-      await queryRunner.query(
-        `ALTER TABLE "interactions" ADD CONSTRAINT "FK_495b59d0dd15e43b262f2da8907" FOREIGN KEY ("interactionOutUUID") REFERENCES "interactions"("uuid") ON DELETE NO ACTION ON UPDATE NO ACTION`
-      );
     }
   }
 
