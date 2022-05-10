fe83cc6d02a21eb17cdde2f616975a60781a129e
Yassine R <riffi.yassine@gmail.com>
2022-05-10 09:59:08 +0200

    fix: correction desm igrations suite au Rebase

--- packages/backend/src/_migrations/1654206241865-auto-migration.ts
+++ packages/backend/src/_migrations/1654206241865-auto-migration.ts
@@ -5,9 +5,6 @@ export class autoMigration1654206241865 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "usager" ALTER COLUMN "preference" SET DEFAULT '{ "phone": false, "phoneNumber": null}'`
-    );
-    await queryRunner.query(
       `ALTER TABLE "usager" ALTER COLUMN "options" SET DEFAULT '{ "transfert":{ "actif":false, "nom":null, "adresse":null, "dateDebut":null, "dateFin":null }, "procurations":[], "npai":{ "actif":false, "dateDebut":null }, "portailUsagerEnabled":false }'`
     );
   }
@@ -15,9 +12,6 @@ export class autoMigration1654206241865 implements MigrationInterface {
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `ALTER TABLE "usager" ALTER COLUMN "options" SET DEFAULT '{"npai": {"actif": false, "dateDebut": null}, "transfert": {"nom": null, "actif": false, "adresse": null, "dateFin": null, "dateDebut": null}, "procurations": [], "portailUsagerEnabled": false}'`
-    );
-    await queryRunner.query(
-      `ALTER TABLE "usager" ALTER COLUMN "preference" SET DEFAULT '{"phone": false, "phoneNumber": null}'`
     );
   }
 }
