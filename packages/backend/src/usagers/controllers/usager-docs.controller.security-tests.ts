a7615ba3c799f3e3b728ca88950089ede0f56498
Yassine R <riffi.yassine@gmail.com>
2022-06-28 15:25:09 +0200

    fix(test): correction du test unitaire

--- packages/backend/src/usagers/controllers/usager-docs.controller.security-tests.ts
+++ packages/backend/src/usagers/controllers/usager-docs.controller.security-tests.ts
@@ -18,14 +18,32 @@ export const UsagerDocsControllerSecurityTests: AppTestHttpClientSecurityTestDef
     {
       label: `${CONTROLLER}.getDocument`,
       query: async (context: AppTestContext) => ({
-        response: await AppTestHttpClient.get("/docs/1/0", {
+        response: await AppTestHttpClient.get(
+          "/docs/1/c669f08b-74a8-4ffc-8128-c9e29e2fd535",
+          {
+            context,
+          }
+        ),
+        expectedStatus: expectedResponseStatusBuilder.allowStructureOnly(
+          context.user,
+          {
+            roles: ["simple", "responsable", "admin"],
+            validExpectedResponseStatus: HttpStatus.BAD_REQUEST, // filesystem document does not exists in tests
+          }
+        ),
+      }),
+    },
+    {
+      label: `${CONTROLLER}.getUsagerDocuments`,
+      query: async (context: AppTestContext) => ({
+        response: await AppTestHttpClient.get("/docs/1", {
           context,
         }),
         expectedStatus: expectedResponseStatusBuilder.allowStructureOnly(
           context.user,
           {
             roles: ["simple", "responsable", "admin"],
-            validExpectedResponseStatus: HttpStatus.BAD_REQUEST, // filesystem document does not exists in tests
+            validExpectedResponseStatus: HttpStatus.OK, // filesystem document does not exists in tests
           }
         ),
       }),
