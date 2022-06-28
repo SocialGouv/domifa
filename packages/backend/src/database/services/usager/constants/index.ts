d368257d99017e4ddb6feac7171afce4330aaaac
Yassine R <riffi.yassine@gmail.com>
2022-06-28 12:47:15 +0200

    fix(tests): mise à jour de tests unitaires

---
+++ packages/backend/src/database/services/usager/constants/index.ts
@@ -0,0 +1,4 @@
+// @index('./*', f => `export * from '${f.path}'`)
+export * from "./USAGER_DEFAULTS.const";
+export * from "./USAGER_LIGHT_ATTRIBUTES.const";
+export * from "./USAGER_PORTAIL_ATTRIBUTES.const";
--- packages/backend/src/database/services/usager/index.ts
+++ packages/backend/src/database/services/usager/index.ts
@@ -1,5 +1,10 @@
+// @index('./*', f => `export * from '${f.path}'`)
+export * from "./constants";
+export * from "./services";
+export * from "./usagerDocsRepository.service";
 export * from "./usagerHistoryRepository.service";
+export * from "./usagerHistoryStatsPeriodRepository.service";
 export * from "./usagerLightRepository.service";
+export * from "./usagerOptionsHistoryRepository.service";
 export * from "./usagerPortailRepository.service";
 export * from "./usagerRepository.service";
-export * from "./usagerOptionsHistoryRepository.service";
