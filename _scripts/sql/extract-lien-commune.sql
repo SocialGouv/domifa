e440a41bda45490bbfb4d26ef1de093e520b9b9f
Yassine R <riffi.yassine@gmail.com>
2022-07-20 16:30:50 +0200

    fix(dumps): suppression de fichiers inutiles fix(docker): Mise à jour de la version docker utilisée localement

--- _scripts/sql/extract-lien-commune.sql
+++
@@ -1,3 +0,0 @@
-SELECT trim(regexp_replace(lower(entretien->>'liencommune'), '[^\w ]+','')) as "liencommune", count(*) FROM public.usager
-where entretien->>'liencommune' is not null and entretien->>'liencommune' <> ''
-group by trim(regexp_replace(lower(entretien->>'liencommune'), '[^\w ]+','')) order by count(*) desc;
