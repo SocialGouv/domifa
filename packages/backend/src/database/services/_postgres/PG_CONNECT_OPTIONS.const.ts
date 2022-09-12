022efdc0277402800610e852858ab7ef2b3c7602
Yassine R <riffi.yassine@gmail.com>
2022-09-12 11:33:25 +0200

    fix(typeorm): correction de la nouvelle connexion en prod

--- packages/backend/src/database/services/_postgres/PG_CONNECT_OPTIONS.const.ts
+++ packages/backend/src/database/services/_postgres/PG_CONNECT_OPTIONS.const.ts
@@ -3,6 +3,43 @@ import { domifaConfig } from "../../../config";
 import { appLogger } from "../../../util";
 import { CustomTypeOrmLogger } from "../../../util/CustomTypeOrmLogger";
 
+const isTypescriptMode = __filename.split(".").pop() === "ts"; // if current file extension is "ts": use src/*.ts files, eles use dist/*.js files
+
+let connectOptionsPaths: Pick<
+  PostgresConnectionOptions,
+  "migrations" | "entities" | "subscribers"
+>;
+
+if (isTypescriptMode) {
+  if (domifaConfig().envId !== "test") {
+    appLogger.warn(`[appTypeormManager] Running in typescript DEV mode`);
+  }
+  connectOptionsPaths = {
+    migrations: domifaConfig().typeorm.createDatabase
+      ? [`src/_migrations/**/*.ts`]
+      : [
+          `src/_migrations/**/*.ts`,
+          `src/_migrations_exclude-from-create_db/**/*.ts`,
+        ],
+    entities: ["src/database/entities/**/*Table.typeorm.ts"],
+    subscribers: ["src/**/*Subscriber.typeorm.ts"],
+  };
+} else {
+  if (domifaConfig().envId !== "test") {
+    appLogger.warn(`[appTypeormManager] Running in javascript DIST mode`);
+  }
+  connectOptionsPaths = {
+    migrations: domifaConfig().typeorm.createDatabase
+      ? [`dist/_migrations/**/*.js`]
+      : [
+          `dist/_migrations/**/*.js`,
+          `dist/_migrations_exclude-from-create_db/**/*.js`,
+        ],
+    entities: ["dist/database/entities/**/*Table.typeorm.js"],
+    subscribers: ["dist/**/*Subscriber.typeorm.js"],
+  };
+}
+
 export const PG_CONNECT_OPTIONS: PostgresConnectionOptions = {
   applicationName: "domifa-api",
   poolErrorHandler: (err: Error) => {
@@ -23,13 +60,6 @@ export const PG_CONNECT_OPTIONS: PostgresConnectionOptions = {
         : ["error", "warn", "info"]
       : false
   ),
-  migrations: domifaConfig().typeorm.createDatabase
-    ? [`src/_migrations/**/*{.ts,.js}`]
-    : [
-        `src/_migrations/*{.ts,.js}`,
-        `src/_migrations/**/*{.ts,.js}`,
-        `src/_migrations_exclude-from-create_db/**/*{.ts,.js}`,
-      ],
-  entities: ["src/database/entities/**/*Table.typeorm{.ts,.js}"],
+  ...connectOptionsPaths,
   maxQueryExecutionTime: 1000,
 };
