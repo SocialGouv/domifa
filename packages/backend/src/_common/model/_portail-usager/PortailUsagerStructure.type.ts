aa3395b388cb0402d593e7783198fef7e1637b60
Yassine R <riffi.yassine@gmail.com>
2022-09-27 18:05:26 +0200

    fix(labels): harmonisation du terme portail usager

--- packages/backend/src/_common/model/_portail-usager/PortailUsagerStructure.type.ts
+++ packages/backend/src/_common/model/_portail-usager/PortailUsagerStructure.type.ts
@@ -3,21 +3,5 @@ import { Structure } from "../structure/Structure.type";
 // PortailUsagerStructure: attributs publics (retournés au Portail Usager via UserStructureAuthenticated)
 export type PortailUsagerStructure = Pick<
   Structure, // @see STRUCTURE_PORTAIL_ATTRIBUTES
-  | "uuid"
-  | "id"
-  // | "adresse"
-  // | "complementAdresse"
-  | "nom"
-  // | "structureType"
-  // | "codePostal"
-  | "ville"
-  // | "departement"
-  // | "region"
-  // | "capacite"
-  // | "agrement"
-  // | "email"
-  // | "responsable"
-  // | "options"
-  // | "adresseCourrier"
-  // | "sms"
+  "uuid" | "id" | "nom" | "ville"
 >;
