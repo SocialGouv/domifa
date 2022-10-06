0e908428891bf92c7621e9a624d2dec8f6708f0e
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-10-06 09:37:19 +0200

    fix(sentry): prevent logging error from /connexion

--- packages/frontend/src/app/app.module.ts
+++ packages/frontend/src/app/app.module.ts
@@ -44,6 +44,8 @@ if (environment.production) {
     dsn: "https://5dab749719e9488798341efad0947291@sentry.fabrique.social.gouv.fr/31",
     environment: environment.env,
     tracesSampleRate: 1.0,
+    // skip errors that produces lots of 401
+    denyUrls: ["/connexion"],
   });
 }
 
