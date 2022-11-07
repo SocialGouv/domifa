c86c6936bd86cf9f0f2a666b46852cdfc5f4400a
Yassine R <riffi.yassine@gmail.com>
2022-11-07 22:29:37 +0100

    fix(lastLogin):	mise à jour de la date de dernière connexion

--- packages/backend/src/auth/services/structures-auth.service.ts
+++ packages/backend/src/auth/services/structures-auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
-import { differenceInDays } from "date-fns";
+import { differenceInCalendarDays } from "date-fns";
 import { structureRepository, userStructureRepository } from "../../database";
 import { appLogger } from "../../util";
 import {
@@ -20,6 +20,7 @@ export const APP_USER_PUBLIC_ATTRIBUTES: (keyof UserStructurePublic)[] = [
   "email",
   "verified",
   "structureId",
+  "lastLogin",
   "fonction",
   "role",
 ];
@@ -63,12 +64,11 @@ export class StructuresAuthService {
     }
 
     // Mise à jour une seule fois par jour
-    const authStructureLastLogin =
-      authUser.structure.lastLogin !== null
-        ? new Date(authUser.structure.lastLogin)
-        : new Date(authUser.structure.createdAt);
+    const authStructureLastLogin = authUser.structure.lastLogin
+      ? new Date(authUser.structure.lastLogin)
+      : new Date(authUser.structure.createdAt);
 
-    if (differenceInDays(authStructureLastLogin, new Date()) > 0) {
+    if (differenceInCalendarDays(authStructureLastLogin, new Date()) < 0) {
       // update structure & user last login date
       await structureRepository.update(
         { id: authUser.structureId },
@@ -76,21 +76,15 @@ export class StructuresAuthService {
       );
     }
 
-    const authUserLastLogin =
-      authUser.lastLogin !== null
-        ? new Date(authUser.lastLogin)
-        : new Date(authUser.createdAt);
+    const authUserLastLogin = authUser.lastLogin
+      ? new Date(authUser.lastLogin)
+      : new Date(authUser.createdAt);
 
     // Mise à jour une seule fois par jour
-    if (differenceInDays(authUserLastLogin, new Date()) > 0) {
+    if (differenceInCalendarDays(authUserLastLogin, new Date()) < 0) {
       await userStructureRepository.updateOne(
-        {
-          id: authUser.id,
-          structureId: authUser.structureId,
-        },
-        {
-          lastLogin: new Date(),
-        }
+        { id: authUser.id, structureId: authUser.structureId },
+        { lastLogin: new Date() }
       );
     }
 
@@ -104,9 +98,7 @@ export class StructuresAuthService {
   ): Promise<UserStructureAuthenticated> {
     const user = await userStructureRepository.findOne<UserStructurePublic>(
       { id: payload._userId },
-      {
-        select: APP_USER_PUBLIC_ATTRIBUTES,
-      }
+      { select: APP_USER_PUBLIC_ATTRIBUTES }
     );
 
     if (typeof user.structureId === "undefined") {
