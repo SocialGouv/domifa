cd74700fd9d76980ea913b226f16738a494a8cda
Yassine R <riffi.yassine@gmail.com>
2022-11-21 16:57:39 +0100

    fix(deps): update node versions

--- packages/backend/src/_migrations/1667855065598-updateLastInteraction-migration.ts
+++ packages/backend/src/_migrations/1667855065598-updateLastInteraction-migration.ts
@@ -3,12 +3,15 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 import { Usager } from "../_common/model";
 import { interactionsCreator } from "../interactions/services";
 
-export class manualMigration1667855065594 implements MigrationInterface {
-  name = "updateLastInteractionMigration1667855065594";
+export class manualMigration1667855065598 implements MigrationInterface {
+  name = "updateLastInteractionMigration1667855065598";
   public async up(queryRunner: QueryRunner): Promise<void> {
     // Recompte des courriers en attente de tous les usagers
-    const usagersToUpdate: Usager[] = await queryRunner.query(
-      `select uuid, "lastInteraction" from usager where "updatedAt" >= '2022-11-16' LIMIT 5000`
+    const usagersToUpdate: Pick<
+      Usager,
+      "uuid" | "updatedAt" | "lastInteraction"
+    >[] = await queryRunner.query(
+      `select uuid, "lastInteraction", "updatedAt" from usager where "updatedAt" BETWEEN '2022-11-16' and '2022-11-17' ORDER by "updatedAt" ASC`
     );
 
     let i = 0;
