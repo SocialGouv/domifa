eb630ab936c6bc53c176340a20285a61c1ccd08a
Yassine R <riffi.yassine@gmail.com>
2023-01-12 14:11:00 +0100

    fix(migration): correction d'une migration bloquante

--- packages/backend/src/_migrations/1673358379788-auto-migration.ts
+++ packages/backend/src/_migrations/1673358379788-auto-migration.ts
@@ -1,9 +1,13 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
+import { domifaConfig } from "../config";
 
 export class autoMigration1673358379788 implements MigrationInterface {
   name = "autoMigration1673358379788";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    if (domifaConfig().envId === "dev" || domifaConfig().envId === "test") {
+      return;
+    }
     await queryRunner.query(
       `ALTER TABLE "user_structure" ADD "acceptTerms" TIMESTAMP WITH TIME ZONE`
     );
