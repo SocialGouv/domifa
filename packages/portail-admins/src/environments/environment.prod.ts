704e7bb22e11dca484c24a4479bd269cf802bdba
Yassine R <riffi.yassine@gmail.com>
2023-05-12 16:51:35 +0200

    fix(sentry): correction de Sentry sur le portail admin

--- packages/portail-admins/src/environments/environment.prod.ts
+++ packages/portail-admins/src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   apiUrl: "${DOMIFA_BACKEND_URL}", // https://domifa-api.fabrique.social.gouv.fr/
   production: true,
   env: "${DOMIFA_ENV_ID}",
+  sentryDsnPortailAdmin: "${DOMIFA_SENTRY_DSN_PORTAIL_ADMIN}",
   healthzCheck: {
     initialCheckDelay: 5,
     checkPeriodIfSuccess: 1800,
