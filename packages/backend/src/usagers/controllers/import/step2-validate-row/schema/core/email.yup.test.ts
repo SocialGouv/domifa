cf3a643224495239b37914c43cb939a69b9d378c
Yassine R <riffi.yassine@gmail.com>
2023-05-30 12:03:38 +0200

    fix(tests): amélioration des tests d'email

--- packages/backend/src/usagers/controllers/import/step2-validate-row/schema/core/email.yup.test.ts
+++ packages/backend/src/usagers/controllers/import/step2-validate-row/schema/core/email.yup.test.ts
@@ -2,15 +2,40 @@ import { email } from "./email.yup";
 
 describe("email schema", () => {
   it("valid email", async () => {
-    expect(await email().validate("name@domain.com")).toEqual(
-      "name@domain.com"
-    );
-    expect(await email().validate("first.last.123@sub.my-domain.com")).toEqual(
-      "first.last.123@sub.my-domain.com"
-    );
+    const validEmails = [
+      "email.complexe+test@gmail.com",
+      "john.doe@example.co.uk",
+      "john_doe123@example.com",
+      "email_123@example-domain.com",
+      "email@subdomain.example.com",
+      "email@maître.fr",
+    ];
+
+    for await (const mail of validEmails) {
+      const result = await email().validate(mail);
+      expect(result).toEqual(mail);
+    }
   });
+
   it("invalid email", async () => {
-    await expect(email().validate("name[AT]domain.com")).rejects.toThrow();
-    await expect(email().validate("name@domain@other.com")).rejects.toThrow();
+    const invalidEmails = [
+      "email_sans_domaine@",
+      ".email_invalide@example.com",
+      "email_invalide@.com",
+      "email@domaine..com",
+      "email#invalide@example.com",
+      "email invalide@example.com",
+      "email@exemple_com",
+      "email@[127.0.0.1]",
+      "email@[IPv6:2001:db8::1]",
+      '"email_invalide"@exemple.com',
+      "email@[ex*mple.com]",
+      "email@[example].com",
+      "email@[example_com]",
+    ];
+
+    for await (const mail of invalidEmails) {
+      await expect(email().validate(mail)).rejects.toThrow();
+    }
   });
 });
--- packages/backend/src/usagers/controllers/import/step2-validate-row/schema/core/email.yup.ts
+++ packages/backend/src/usagers/controllers/import/step2-validate-row/schema/core/email.yup.ts
@@ -1,5 +1,6 @@
 import * as yup from "yup";
+import { ValidationRegexp } from "./ValidationRegexp.data";
 
 export function email() {
-  return yup.string().trim().email();
+  return yup.string().trim().email().matches(ValidationRegexp.email);
 }
