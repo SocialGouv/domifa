e8bdba9ce5ac555aa49784f2cb04249d98b8aa6d
Yassine R <riffi.yassine@gmail.com>
2023-07-05 14:14:54 +0200

    fix(sonarcloud): ajout de règles

--- packages/backend/src/usagers/controllers/import/step2-validate-row/schema/test-data/index.ts
+++ packages/backend/src/usagers/controllers/import/step2-validate-row/schema/test-data/index.ts
@@ -1,5 +1,5 @@
 // @index('./*', f => `export * from '${f.path}'`)
-export * from "./TEST_INVALID_IMPORT_USAGER.const";
-export * from "./TEST_VALID_IMPORT_ROW.const";
-export * from "./TEST_VALID_IMPORT_USAGER_REFUS.const";
-export * from "./TEST_VALID_IMPORT_USAGER.const";
+export * from "./TEST_INVALID_IMPORT_USAGER.mock";
+export * from "./TEST_VALID_IMPORT_ROW.mock";
+export * from "./TEST_VALID_IMPORT_USAGER_REFUS.mock";
+export * from "./TEST_VALID_IMPORT_USAGER.mock";
