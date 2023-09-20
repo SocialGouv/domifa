b0faa353f3d6a672f0ee7fa553d8594ce5e8d9d7
Yassine R. <riffi.yassine@gmail.com>
2023-09-21 01:09:55 +0200

    fix(api): add location to structures

--- packages/backend/src/_migrations/1695043582855-auto-migration.ts
+++ packages/backend/src/_migrations/1695043582855-auto-migration.ts
@@ -1,16 +1,19 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
+import { EXECUTE_MIGRATIONS } from "../database";
 
 export class AutoMigration1695043582855 implements MigrationInterface {
   name = "AutoMigration1695043582855";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    console.log("[MIGRATION] Création de 'location' dans les structures");
-    await queryRunner.query(
-      `ALTER TABLE "structure" ADD "latitude" double precision`
-    );
-    await queryRunner.query(
-      `ALTER TABLE "structure" ADD "longitude" double precision`
-    );
+    if (EXECUTE_MIGRATIONS) {
+      console.log("[MIGRATION] Création de 'location' dans les structures");
+      await queryRunner.query(
+        `ALTER TABLE "structure" ADD "latitude" double precision`
+      );
+      await queryRunner.query(
+        `ALTER TABLE "structure" ADD "longitude" double precision`
+      );
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
