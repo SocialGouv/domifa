806fdc32e40bcdd3b27a408bfe24b067f43432d0
Yassine R <riffi.yassine@gmail.com>
2023-09-26 16:44:42 +0200

    fix(structures): add position to structures

--- packages/backend/src/database/services/_postgres/EXECUTE_MIGRATIONS.const.ts
+++
@@ -1,7 +0,0 @@
-import { domifaConfig } from "../../../config";
-import { isCronEnabled } from "../../../config/services/isCronEnabled.service";
-
-export const EXECUTE_MIGRATIONS =
-  ((domifaConfig().envId === "preprod" || domifaConfig().envId === "prod") &&
-    isCronEnabled()) ||
-  domifaConfig().envId === "local";
--- packages/backend/src/database/services/_postgres/index.ts
+++ packages/backend/src/database/services/_postgres/index.ts
@@ -1,6 +1,5 @@
 // @index('./*', f => `export * from '${f.path}'`)
 export * from "./appTypeormManager.service";
-export * from "./EXECUTE_MIGRATIONS.const";
 export * from "./PG_CONNECT_OPTIONS.const";
 export * from "./PG_MIGRATION_CONFIG.const";
 export * from "./pgRepository.service";
