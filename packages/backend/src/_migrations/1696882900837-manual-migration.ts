7fb2e4d1a95b8f2d4c84c4e73bb5be7e09cc9c62
Yassine R <riffi.yassine@gmail.com>
2023-10-10 15:57:48 +0200

    fix(api): fix migrations

--- packages/backend/src/_migrations/1696882900837-manual-migration.ts
+++ packages/backend/src/_migrations/1696882900837-manual-migration.ts
@@ -2,50 +2,57 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 import { usagerRepository } from "../database";
 import { Usager } from "../_common/model";
+import { domifaConfig } from "../config";
 
 export class ManualMigration1696882900837 implements MigrationInterface {
   public async up(_queryRunner: QueryRunner): Promise<void> {
-    const usagers: Usager[] = await usagerRepository
-      .createQueryBuilder("usager")
-      .select("*")
-      .where(
-        `(decision->>'dateFin' is null OR decision->>'dateDebut' is null) and decision->>'statut' not in ('INSTRUCTION', 'ATTENTE_DECISION')`
-      )
-      .getRawMany();
+    if (
+      domifaConfig().envId === "prod" ||
+      domifaConfig().envId === "preprod" ||
+      domifaConfig().envId === "local"
+    ) {
+      const usagers: Usager[] = await usagerRepository
+        .createQueryBuilder("usager")
+        .select("*")
+        .where(
+          `(decision->>'dateFin' is null OR decision->>'dateDebut' is null) and decision->>'statut' not in ('INSTRUCTION', 'ATTENTE_DECISION')`
+        )
+        .getRawMany();
 
-    for (const usager of usagers) {
-      if (usager.decision.dateDebut && !usager.decision.dateFin) {
-        usager.decision.dateFin = usager.decision.dateDebut;
-      } else if (!usager.decision.dateDebut && usager.decision.dateFin) {
-        usager.decision.dateDebut = usager.decision.dateFin;
-      } else if (usager.decision.dateDecision) {
-        usager.decision.dateFin = usager.decision.dateDecision;
-        usager.decision.dateDebut = usager.decision.dateDecision;
-      }
-      if (!usager.decision.dateDecision) {
-        usager.decision.dateDecision = usager.decision.dateDebut;
-      }
+      for (const usager of usagers) {
+        if (usager.decision.dateDebut && !usager.decision.dateFin) {
+          usager.decision.dateFin = usager.decision.dateDebut;
+        } else if (!usager.decision.dateDebut && usager.decision.dateFin) {
+          usager.decision.dateDebut = usager.decision.dateFin;
+        } else if (usager.decision.dateDecision) {
+          usager.decision.dateFin = usager.decision.dateDecision;
+          usager.decision.dateDebut = usager.decision.dateDecision;
+        }
+        if (!usager.decision.dateDecision) {
+          usager.decision.dateDecision = usager.decision.dateDebut;
+        }
 
-      if (!usager.decision.dateDecision) {
-        usager.decision.dateFin = usager.createdAt;
-        usager.decision.dateDebut = usager.createdAt;
-        usager.decision.dateDecision = usager.createdAt;
-      }
+        if (!usager.decision.dateDecision) {
+          usager.decision.dateFin = usager.createdAt;
+          usager.decision.dateDebut = usager.createdAt;
+          usager.decision.dateDecision = usager.createdAt;
+        }
 
-      usager.historique[usager.historique.length - 1] = usager.decision;
+        usager.historique[usager.historique.length - 1] = usager.decision;
 
-      if (
-        usager.decision.statut &&
-        usager.decision.dateDebut &&
-        usager.decision.dateFin
-      ) {
-        await usagerRepository.update(
-          { uuid: usager.uuid },
-          {
-            decision: usager.decision,
-            historique: usager.historique,
-          }
-        );
+        if (
+          usager.decision.statut &&
+          usager.decision.dateDebut &&
+          usager.decision.dateFin
+        ) {
+          await usagerRepository.update(
+            { uuid: usager.uuid },
+            {
+              decision: usager.decision,
+              historique: usager.historique,
+            }
+          );
+        }
       }
     }
   }
