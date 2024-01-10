b208f352c3b326ad651564d92d29fb4761ff416b
Yassine R. <riffi.yassine@gmail.com>
2024-01-10 22:37:04 +0100

    fix(stats): order of stats

--- packages/backend/src/excel/export-structure-stats/worksheet-renderer/exportStructureStatsWorksheetSection1Renderer.ts
+++ packages/backend/src/excel/export-structure-stats/worksheet-renderer/exportStructureStatsWorksheetSection1Renderer.ts
@@ -129,6 +129,29 @@ function renderSection1ValidUsagers(context: {
   i++; // blank line
   i++; // title
   worksheetRendered.renderCell(i++, "c", {
+    value: data.liencommune.residentiel,
+  });
+  worksheetRendered.renderCell(i++, "c", {
+    value: data.liencommune.parental,
+  });
+  worksheetRendered.renderCell(i++, "c", {
+    value: data.liencommune.familial,
+  });
+  worksheetRendered.renderCell(i++, "c", {
+    value: data.liencommune.professionnel,
+  });
+  worksheetRendered.renderCell(i++, "c", {
+    value: data.liencommune.social,
+  });
+  worksheetRendered.renderCell(i++, "c", {
+    value: data.liencommune.autre,
+  });
+  worksheetRendered.renderCell(i++, "c", {
+    value: data.liencommune.non_renseigne,
+  });
+  i++; // blank line
+  i++; // title
+  worksheetRendered.renderCell(i++, "c", {
     value: data.menage.couple_avec_enfant,
   });
   worksheetRendered.renderCell(i++, "c", {
