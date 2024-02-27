d44f24b1f142a72021a015b44c0fd14ccebcc0a9
Yassine R <riffi.yassine@gmail.com>
2024-02-27 18:04:14 +0100

    fix(import): delete useless files

--- packages/backend/src/_common/tmp-toulouse/index.ts
+++
@@ -1,5 +0,0 @@
-// @index('./*', f => `export * from '${f.path}'`)
-export * from "./getDateFromXml";
-export * from "./PAYS";
-export * from "./TOULOUSE_STRUCTURE_ID.const";
-export * from "./TUsager.interface";
