d8f8d304e0f29831fca9b52acd09047d6c579178
Adrien Chauve <adrien@chauve.eu>
2024-04-16 16:39:54 +0200

    WIP

---
+++ .anonymizer/anonymize.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+set -e
+
+./greenmask dump --config anonymize_config.yaml
+
+# export PGHOST=localhost
+# export PGUSER=domifa_user
+# export PGPASSWORD=pizza
+# export PGDATABASE=domifa_dev_anonymized
+# pg_restore --clean --if-exists --no-owner --no-acl --verbose -d domifa_dev_anonymized tmp/pg_dump/$(ls tmp/pg_dump/ | tail -n 1)
