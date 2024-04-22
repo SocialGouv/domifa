7b6ba3e467f3cbc551687436d01ba43784608008
Yassine R. <riffi.yassine@gmail.com>
2024-04-22 22:38:13 +0200

    fix(backend): update migration

--- packages/backend/src/_migrations/1713285228205-auto-migration.ts
+++ packages/backend/src/_migrations/1713285228205-auto-migration.ts
@@ -7,7 +7,6 @@ export class AutoMigration1713285228205 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     if (
       domifaConfig().envId === "prod" ||
-      domifaConfig().envId === "preprod" ||
       domifaConfig().envId === "dev" ||
       domifaConfig().envId === "test"
     ) {
