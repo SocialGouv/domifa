fcddc99219782a3a8a647ce2eb2af97f705e8c8d
Adrien Chauve <adrien@chauve.eu>
2024-05-07 16:44:53 +0200

    WIP usager

--- .anonymizer/anonymize.sh
+++ .anonymizer/anonymize.sh
@@ -12,16 +12,9 @@ export PGUSER=domifa_user
 export PGPASSWORD=pizza
 export PGDATABASE=domifa_dev
 
-$directory/greenmask -v || (echo "You need to download greenmask from https://github.com/GreenmaskIO/greenmask/releases/tag/v0.1.9 in $directory" && exit)
+greenmask -v || (echo "You need to download greenmask from https://github.com/GreenmaskIO/greenmask/releases/tag/v0.1.13 in $directory" && exit)
 
-sed "s%/tmp/pg_dump%$directory/tmp/pg_dump%" $directory/config.yaml > $directory/config.local.yaml
-$directory/greenmask dump --config $directory/config.local.yaml
 
-export PGHOST=localhost
-export PGUSER=domifa_user
-export PGPASSWORD=pizza
-export PGDATABASE=domifa_dev_anonymized
-pg_restore --clean --if-exists --no-owner --no-acl --verbose -d $PGDATABASE $pg_dump_directory/$(ls $pg_dump_directory | tail -n 1)
 
 cat $directory/config.yaml \
     | sed "s%/tmp/pg_dump%$directory/tmp/pg_dump%" \
@@ -32,4 +25,11 @@ cat $directory/config.yaml \
     | sed "s%{{bucket_region}}%$bucket_region%" \
     > $directory/config.local.yaml
 
-$directory/greenmask dump --config $directory/config.local.yaml
+greenmask dump --config $directory/config.local.yaml
+
+export PGHOST=localhost
+export PGUSER=domifa_user
+export PGPASSWORD=pizza
+export PGDATABASE=domifa_dev_anonymized
+pg_restore --clean --if-exists --no-owner --no-acl --verbose -d $PGDATABASE $pg_dump_directory/$(ls $pg_dump_directory | tail -n 1)
+
