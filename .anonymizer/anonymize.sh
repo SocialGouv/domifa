28d592944b92d8675b8240f83830e2283fbccaac
Adrien Chauve <adrien@chauve.eu>
2024-05-07 15:03:31 +0200

    add upload dump to s3

--- .anonymizer/anonymize.sh
+++ .anonymizer/anonymize.sh
@@ -23,4 +23,13 @@ export PGPASSWORD=pizza
 export PGDATABASE=domifa_dev_anonymized
 pg_restore --clean --if-exists --no-owner --no-acl --verbose -d $PGDATABASE $pg_dump_directory/$(ls $pg_dump_directory | tail -n 1)
 
+cat $directory/config.yaml \
+    | sed "s%/tmp/pg_dump%$directory/tmp/pg_dump%" \
+    | sed "s%{{bucket_access_key}}%$bucket_access_key%" \
+    | sed "s%{{bucket_secret_key}}%$bucket_secret_key%" \
+    | sed "s%{{bucket_endpoint}}%$bucket_endpoint%" \
+    | sed "s%{{bucket_name}}%$bucket_name%" \
+    | sed "s%{{bucket_region}}%$bucket_region%" \
+    > $directory/config.local.yaml
 
+$directory/greenmask dump --config $directory/config.local.yaml
