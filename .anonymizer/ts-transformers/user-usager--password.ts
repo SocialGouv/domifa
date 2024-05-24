0431ebbc075ef82aeaf30379472903d0c95ec304
Adrien Chauve <adrien@chauve.eu>
2024-05-24 19:41:20 +0200

    fix: hash

--- .anonymizer/ts-transformers/user-usager--password.ts
+++ .anonymizer/ts-transformers/user-usager--password.ts
@@ -1,11 +1,10 @@
-import { hash } from "bcryptjs";
+import { hashSync } from "bcryptjs";
 import { main } from "./common/lib"
 
-const __HASHED__PASSWORD__ = hash("DOMIFA_ANONYMIZER_PASSWORD", 10)
+const __HASHED__PASSWORD__ = hashSync("DOMIFA_ANONYMIZER_PASSWORD", 10)
 
 function anonymize(values: Record<string, any>) {
   values.password = __HASHED__PASSWORD__
-  process.stderr.write(JSON.stringify(values))
 }
 
 main(["password"], anonymize)
