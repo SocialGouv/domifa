d438793566e6b525d5ffe58600f7e93c7a15eddf
Adrien Chauve <adrien@chauve.eu>
2024-06-11 10:23:10 +0200

    feat: add anonymization

---
+++ .anonymizer/ts-transformers/common/lib.ts
@@ -0,0 +1,46 @@
+export function main<T extends { d: any }>(
+  columnNames: string[],
+  anonymize: (columns: Record<string, T[]>) => void
+) {
+  const stdin = process.stdin
+  const stdout = process.stdout
+  const stderr = process.stderr
+
+  stderr.write("Anonymizer started\n")
+
+  process.once("SIGTERM", () => {
+    stderr.write("SIGTERM received\n")
+  })
+
+  stdin.on("data", function (lineBuffer) {
+    try {
+      const line: Record<string, T> = JSON.parse(lineBuffer.toString())
+      const columns = Object.fromEntries(
+        Object.entries(line).map(([columnName, columnValue]) => [
+          columnName,
+          columnValue.d,
+        ])
+      )
+      anonymize(columns)
+      const anonymizedLine = Object.fromEntries(
+        Object.entries(columns).map(([columnName, columnValue]) => [
+          columnName,
+          { d: columnValue },
+        ])
+      )
+      stdout.write(JSON.stringify(anonymizedLine) + "\n")
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        stderr.write(`Error: ${error.message}\n`)
+        stderr.write(`Stack: ${error.stack}\n`)
+      } else {
+        stderr.write(`Error: ${error}\n`)
+      }
+      process.exit(1)
+    }
+  })
+
+  stdin.on("end", function () {
+    stderr.write("Anonymizer ended\n")
+  })
+}
---
+++ .anonymizer/ts-transformers/usager-notes--all-columns.ts
@@ -0,0 +1,22 @@
+import { main } from "./common/lib"
+import { randomInt, fullName } from "./common/data-helpers"
+import { fakerFR as faker } from "@faker-js/faker"
+
+function anonymize(values: Record<string, any>) {
+  values.message = faker.lorem.sentence(5)
+  values.createdBy = JSON.stringify({
+    userId: randomInt(),
+    userName: fullName(),
+  })
+
+  if (values.archived) {
+    values.archivedBy = JSON.stringify({
+      userId: randomInt(),
+      userName: fullName(),
+    })
+  } else {
+    values.archivedBy = JSON.stringify(null)
+  }
+}
+
+main(["archived", "message", "createdBy", "archivedBy"], anonymize)
