795bb87356dfed54ca8d870e4d3d0a93f63f2a4e
Yassine R. <riffi.yassine@gmail.com>
2024-06-12 18:22:50 +0200

    fix(admin): fix unit test

--- packages/portail-admins/src/app/modules/admin-structures/components/national-stats/national-stats.component.spec.ts
+++ packages/portail-admins/src/app/modules/admin-structures/components/national-stats/national-stats.component.spec.ts
@@ -4,6 +4,7 @@ import { NationalStatsComponent } from "./national-stats.component";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
+import { MATOMO_INJECTORS } from "../../../../shared/MATOMO_INJECTORS.const";
 
 describe("NationalStatsComponent", () => {
   let component: NationalStatsComponent;
@@ -12,8 +13,9 @@ describe("NationalStatsComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [NationalStatsComponent],
-      imports: [RouterTestingModule, HttpClientTestingModule],
+      imports: [MATOMO_INJECTORS, RouterTestingModule, HttpClientTestingModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+      providers: [],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NationalStatsComponent);
