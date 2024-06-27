1010af8eaba76e6588ca89e9a96eefab3e940834
Yassine R. <riffi.yassine@gmail.com>
2024-06-27 15:34:31 +0200

    fix(frontend): fix landing page for mon domifa

--- packages/frontend/src/app/modules/general/components/static-pages/landing-page-portail/landing-page-portail.component.spec.ts
+++ packages/frontend/src/app/modules/general/components/static-pages/landing-page-portail/landing-page-portail.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { LandingPagePortailComponent } from "./landing-page-portail.component";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { RouterTestingModule } from "@angular/router/testing";
+import { APP_BASE_HREF } from "@angular/common";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 
 describe("LandingPagePortailComponent", () => {
   let component: LandingPagePortailComponent;
@@ -9,6 +13,9 @@ describe("LandingPagePortailComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LandingPagePortailComponent],
+      imports: [RouterTestingModule, NoopAnimationsModule],
+      providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LandingPagePortailComponent);
