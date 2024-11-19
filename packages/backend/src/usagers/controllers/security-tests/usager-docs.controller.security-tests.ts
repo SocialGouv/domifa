a6ce0f016648b89c0a6496b0d63683622fb664c0
Yassine R. <riffi.yassine@gmail.com>
2024-11-19 16:38:31 +0100

    fix(backend): fix unit test for getUsagerDocs

--- packages/backend/src/usagers/controllers/security-tests/usager-docs.controller.security-tests.ts
+++ packages/backend/src/usagers/controllers/security-tests/usager-docs.controller.security-tests.ts
@@ -44,7 +44,6 @@ export const UsagerDocsControllerSecurityTests: AppTestHttpClientSecurityTestDef
           context.user,
           {
             roles: ["simple", "responsable", "admin"],
-            validStructureIds: [1],
             validExpectedResponseStatus: HttpStatus.OK, // filesystem document does not exists in tests
             invalidStructureIdExpectedResponseStatus: HttpStatus.BAD_REQUEST,
           }
