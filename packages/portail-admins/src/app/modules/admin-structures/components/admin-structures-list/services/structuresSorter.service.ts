90f5631d6772e9720ca89770c1f94dea8f4c4663
Yassine R. <riffi.yassine@gmail.com>
2025-01-09 23:56:59 +0100

    fix(admin): fix export in admin

--- packages/portail-admins/src/app/modules/admin-structures/components/admin-structures-list/services/structuresSorter.service.ts
+++ packages/portail-admins/src/app/modules/admin-structures/components/admin-structures-list/services/structuresSorter.service.ts
@@ -20,6 +20,14 @@ function sortBy(
     return structures;
   }
 
+  if (["id", "users", "actifs", "usagers"].includes(sortKey)) {
+    return structures.sort((a, b) => {
+      const valueA = a[sortKey as keyof StructureAdmin] as number;
+      const valueB = b[sortKey as keyof StructureAdmin] as number;
+      return sortNumeric(valueA, valueB, asc);
+    });
+  }
+
   return sortMultiple(structures, asc, (structure) => {
     const sortAttributes: SortableValue[] = [];
 
@@ -32,11 +40,13 @@ function sortBy(
     ) {
       value = normalizeString(value as string);
     }
-
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     sortAttributes.push(value as any);
-    sortAttributes.push(normalizeString(structure.nom));
-
+    sortAttributes.push(structure.nom);
     return sortAttributes;
   });
 }
+
+export const sortNumeric = (a: number, b: number, asc: boolean): number => {
+  return asc ? a - b : b - a;
+};
