a1581f356e8de2f1580e002495f118835a38954b
Yassine R. <riffi.yassine@gmail.com>
2025-02-16 23:27:17 +0100

    fix(backend): fix unit tests

--- packages/frontend/src/app/modules/manage-usagers/components/assign-referrers/assign-referrers.component.spec.ts
+++ packages/frontend/src/app/modules/manage-usagers/components/assign-referrers/assign-referrers.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { AssignReferrersComponent } from "./assign-referrers.component";
+import { APP_BASE_HREF } from "@angular/common";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { StoreModule } from "@ngrx/store";
+import { _usagerReducer } from "../../../../shared";
 
 describe("AssignReferrersComponent", () => {
   let component: AssignReferrersComponent;
@@ -9,10 +15,20 @@ describe("AssignReferrersComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AssignReferrersComponent],
+      imports: [
+        NgbModule,
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientTestingModule,
+        StoreModule.forRoot({ app: _usagerReducer }),
+      ],
+      providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AssignReferrersComponent);
     component = fixture.componentInstance;
+    component.selectedRefs = new Set<number>();
+    component.newReferrerId = null;
     fixture.detectChanges();
   });
 
--- packages/frontend/src/app/modules/shared/components/input-referrer/input-referrer.component.spec.ts
+++ packages/frontend/src/app/modules/shared/components/input-referrer/input-referrer.component.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { InputReferrerComponent } from "./input-referrer.component";
+import { APP_BASE_HREF } from "@angular/common";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { RouterTestingModule } from "@angular/router/testing";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { StoreModule } from "@ngrx/store";
+import { _usagerReducer } from "../../../../shared";
 
 describe("InputReferrerComponent", () => {
   let component: InputReferrerComponent;
@@ -9,10 +17,21 @@ describe("InputReferrerComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [InputReferrerComponent],
+      imports: [
+        NgbModule,
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+        StoreModule.forRoot({ app: _usagerReducer }),
+      ],
+      providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(InputReferrerComponent);
     component = fixture.componentInstance;
+    component.submitted = true;
     fixture.detectChanges();
   });
 
