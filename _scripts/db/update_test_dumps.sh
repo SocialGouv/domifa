155e4ea3c1c900fb999c5c6337a0e6fb97b5cca6
Yassine R. <riffi.yassine@gmail.com>
2025-07-18 18:44:26 +0200

    fix(backend): fix script to export test's dump

--- _scripts/db/update_test_dumps.sh
+++ _scripts/db/update_test_dumps.sh
@@ -12,6 +12,6 @@ ${CURRENT_DIR}/make-dump-database-docker.sh --db=test
 ${CURRENT_DIR}/make-dump-data-only-docker.sh --db=test --exclude-table=migrations
 
 # generate truncate file automatically
-${CURRENT_DIR}/generate-truncate.sh --db=test
+${CURRENT_DIR}/generate-trucante-tables.sh --db=test
 
 cat ${DUMPS_DIR}/domifa_test.postgres.truncate-data.sql ${DUMPS_DIR}/domifa_test.postgres.restore-data-only.sql >${DUMPS_DIR}/domifa_test.postgres.truncate-restore-data-only.sql
