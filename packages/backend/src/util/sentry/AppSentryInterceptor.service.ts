640370ac29f44014c9a7352273c6fc9f7fecf766
Yassine R. <riffi.yassine@gmail.com>
2025-08-11 17:52:24 +0200

    fix(ci): update sentry dsn

--- packages/backend/src/util/sentry/AppSentryInterceptor.service.ts
+++ packages/backend/src/util/sentry/AppSentryInterceptor.service.ts
@@ -1,105 +1,60 @@
 import {
-  CallHandler,
-  ExecutionContext,
   Injectable,
-  InternalServerErrorException,
   NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  InternalServerErrorException,
 } from "@nestjs/common";
-import { addRequestDataToEvent, Request as SentryRequest } from "@sentry/node";
-
-import { Observable, throwError } from "rxjs";
-import { catchError } from "rxjs/operators";
-import { UserStructureAuthenticated } from "../../_common/model";
-import { Request } from "express";
+import { getCurrentScope, captureException } from "@sentry/node";
+import { Observable, catchError, throwError } from "rxjs";
 import { appLogger } from "../logs";
+import {
+  UserStructureAuthenticated,
+  UserUsagerAuthenticated,
+} from "../../_common/model";
+import { UserSupervisorAuthenticated } from "../../_common/model/users/user-supervisor";
 
 @Injectable()
 export class AppSentryInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    // https://docs.nestjs.com/interceptors#exception-mapping
     return next.handle().pipe(
       catchError((err) => {
-        try {
-          let prefix: string;
-          const logContext: Record<string, any> = {};
+        const {
+          user,
+        }: {
+          user:
+            | UserStructureAuthenticated
+            | UserSupervisorAuthenticated
+            | UserUsagerAuthenticated;
+        } = context.switchToHttp().getRequest();
 
-          if (context.getType() === "http") {
-            prefix = "[http]";
-            const { req, user } = parseRequest(context);
-            if (req) {
-              logContext.req = logSentryRequest(req);
-            }
-            if (user) {
-              logContext.user = logSentryUser(user);
-            }
-          } else {
-            prefix = "[core]";
-          }
-
-          appLogger.error(
-            `${prefix} ${
-              err.message ?? "unexpected error"
-            } ${new Date().toUTCString()}`,
-            { error: err, sentry: true, context: logContext }
-          );
-        } catch (error) {
-          appLogger.error(
-            "[AppSentryInterceptor] Unexpected error while processing sentry event",
-            { error, sentry: true }
-          );
+        if (user?._userProfile === "structure") {
+          getCurrentScope().setUser({
+            id: user?.id,
+            role: user?.role,
+            userProfile: "structure",
+            structureId: user?.structure,
+          });
+        } else if (user?._userProfile === "supervisor") {
+          getCurrentScope().setUser({
+            id: user?.id,
+            role: user?.role,
+            userProfile: user._userProfile,
+          });
+        } else if (user?._userProfile === "usager") {
+          getCurrentScope().setUser({
+            uuid: user.usager.uuid,
+            id: user._userId,
+            userProfile: user._userProfile,
+          });
         }
+
+        captureException(err);
+
+        appLogger.error(`[http] ${err.message}`, { error: err, sentry: true });
+
         return throwError(() => new InternalServerErrorException());
       })
     );
   }
-}
-
-function parseRequest(context: ExecutionContext): {
-  req: SentryRequest;
-  user: UserStructureAuthenticated;
-} {
-  const httpContext = context.switchToHttp();
-  const expressRequest: Request = httpContext.getRequest();
-
-  if (!expressRequest) {
-    return null;
-  }
-  const data = addRequestDataToEvent({}, expressRequest, {
-    include: {
-      request: true,
-      user: false,
-      transaction: true,
-    },
-  });
-  const req = data.request;
-  const user = expressRequest.user as UserStructureAuthenticated;
-
-  return {
-    req,
-    user,
-  };
-}
-
-function logSentryRequest(req: any): Record<string, any> {
-  const headers = req.headers ?? {};
-  return {
-    method: req.method,
-    url: req.url,
-    data: req?.data,
-    headers: {
-      host: headers.host,
-      origin: headers.origin,
-      referer: headers.referer,
-      "user-agent": req.headers["user-agent"],
-      withAuthorizationToken: headers.authorization !== undefined,
-    },
-  };
-}
-
-function logSentryUser(user: UserStructureAuthenticated): Record<string, any> {
-  return {
-    id: user.id,
-    role: user.role,
-    structureId: user.structureId,
-  };
 }
