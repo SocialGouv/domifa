94c6c0fc68d342216d51b9307006bfdb0be4f1bd
Yassine R. <riffi.yassine@gmail.com>
2025-08-18 17:20:09 +0200

    fix(yarn): fix yarn.lock

--- packages/portail-admins/jest.config.js
+++ packages/portail-admins/jest.config.js
@@ -7,4 +7,7 @@ module.exports = {
     "^assets/(.*)$": "<rootDir>/src/assets/$1",
     "^environments/(.*)$": "<rootDir>/src/environments/$1",
   },
+  transformIgnorePatterns: [
+    "node_modules/(?!.*\\.mjs$|uuid|@edugouvfr/ngx-dsfr)",
+  ],
 };
