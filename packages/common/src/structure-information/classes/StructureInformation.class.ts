ff9fb7047d7e8246b3ee8933be0917b40b3a2bee
hbalty <balty.houssem@gmail.com>
2025-09-17 12:42:11 +0200

    fix(backend): optional chaining

--- packages/common/src/structure-information/classes/StructureInformation.class.ts
+++ packages/common/src/structure-information/classes/StructureInformation.class.ts
@@ -17,26 +17,27 @@ export class StructureInformation implements AppEntity {
   public createdAt?: Date;
   public updatedAt?: Date;
   public version?: number;
-  constructor(options: Partial<StructureInformation>) {
-    this.title = options.title || "";
-    this.description = options.description || "";
-    this.startDate = options.startDate || null;
-    this.endDate = options.endDate || null;
-    this.type = options.type || "other";
-    this.createdAt = createDate(options.createdAt) ?? new Date();
-    this.updatedAt = createDate(options.updatedAt) ?? new Date();
-    this.createdBy = options.createdBy;
-    this.structureId = options.structureId;
-    this.isTemporary = options.isTemporary;
+  constructor(options?: Partial<StructureInformation>) {
+    this.title = options?.title || "";
+    this.description = options?.description || "";
+    this.startDate = options?.startDate || null;
+    this.endDate = options?.endDate || null;
+    this.type = options?.type || "other";
+    this.createdAt = createDate(options?.createdAt) ?? new Date();
+    this.updatedAt = createDate(options?.updatedAt) ?? new Date();
+    this.createdBy = options?.createdBy;
+    this.structureId = options?.structureId;
+    this.isTemporary = options?.isTemporary;
     this.isExpired = isMessageExpired(options);
-    this.uuid = options.uuid;
-    this.version = options.version;
+    this.uuid = options?.uuid;
+    this.version = options?.version;
   }
 }
 
 export function isMessageExpired(
-  message: Partial<StructureInformation>
-): boolean {
+  message?: Partial<StructureInformation>
+): boolean | undefined {
+  if (!message) return undefined;
   if (!message.endDate || !message.startDate || !message.isTemporary)
     return false;
   const today = new Date();
