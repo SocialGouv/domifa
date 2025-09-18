8e2c517c14cf40dc3e1a02dcac246b964b5fd955
hbalty <balty.houssem@gmail.com>
2025-09-18 15:47:45 +0200

    fix(portail-admin): fix tests unitaire

--- packages/portail-admins/jest.config.js
+++ packages/portail-admins/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: "jest-preset-angular",
-  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
   moduleNameMapper: {
     "^src/(.*)$": "<rootDir>/src/$1",
     "^app/(.*)$": "<rootDir>/src/app/$1",
