c1766624267add0f25248648d75e1a64258e813b
hbalty <balty.houssem@gmail.com>
2025-09-18 16:46:48 +0200

    fix(portails-usager): fix version fa

--- packages/portail-admins/jest.config.js
+++ packages/portail-admins/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
   moduleNameMapper: {
     "^src/(.*)$": "<rootDir>/src/$1",
     "^app/(.*)$": "<rootDir>/src/app/$1",
