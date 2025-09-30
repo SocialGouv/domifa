2803a967aa1714a7f6bc8bd2d0170bedbe401196
hbalty <balty.houssem@gmail.com>
2025-09-30 13:36:19 +0200

    fix(usagers): fix reviews

--- packages/portail-usagers/src/app/modules/shared/components/faq/faq.component.ts
+++ packages/portail-usagers/src/app/modules/shared/components/faq/faq.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 import { DsfrAccordionModule, DsfrButtonModule } from "@edugouvfr/ngx-dsfr";
 
 @Component({
   selector: "app-faq",
   standalone: true,
-  imports: [DsfrAccordionModule, DsfrButtonModule],
+  imports: [DsfrAccordionModule, DsfrButtonModule, RouterModule],
   templateUrl: "./faq.component.html",
   styleUrl: "./faq.component.css",
 })
