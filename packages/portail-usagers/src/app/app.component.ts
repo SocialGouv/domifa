1e13ad84f28d172db60b8e53abcd3975e513a0f8
hbalty <balty.houssem@gmail.com>
2025-10-15 14:07:19 +0200

    feat(usagers): adding home banner + using button for logout

--- packages/portail-usagers/src/app/app.component.ts
+++ packages/portail-usagers/src/app/app.component.ts
@@ -27,7 +27,6 @@ export class AppComponent implements OnInit {
   public skipLinks: DsfrLink[] = [];
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public news: any;
-  public headerToolsLinks: DsfrLink[] = [];
   @ViewChild("newsModal", { static: true })
   public newsModal!: TemplateRef<NgbModalRef>;
 
@@ -109,15 +108,6 @@ export class AppComponent implements OnInit {
         this.usagerProfile = usager;
 
         if (usager) {
-          this.headerToolsLinks = [
-            {
-              ariaControls: "logoutModal",
-              linkId: "logout",
-              mode: "button",
-              label: "Se déconnecter",
-              icon: "fr-icon-logout-box-r-line",
-            },
-          ];
           this.checkNews();
         }
       }
@@ -159,8 +149,6 @@ export class AppComponent implements OnInit {
   }
 
   public logout(): void {
-    // if more buttons are added to headerToolsLinks, change the logic to filter to only log when logout event is triggered
-    this.headerToolsLinks = [];
     this.usagerAuthService.logoutAndRedirect();
   }
 }
