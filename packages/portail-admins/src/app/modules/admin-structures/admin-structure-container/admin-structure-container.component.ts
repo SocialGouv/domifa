379494b76e8995c9cc8d3c92a4f56c690c2db6fe
hbalty <balty.houssem@gmail.com>
2025-10-31 00:09:21 +0100

    feat(admins): refactoring routing structure

---
+++ packages/portail-admins/src/app/modules/admin-structures/admin-structure-container/admin-structure-container.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-admin-structure-container",
+  templateUrl: "./admin-structure-container.component.html",
+  styleUrl: "./admin-structure-container.component.css",
+})
+export class AdminStructureContainerComponent {}
