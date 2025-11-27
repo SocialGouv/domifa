26276d2aa9f362d0664b00c07403e0ca7e0cdabc
Houssem Balty <balty.houssem@gmail.com>
2025-11-27 17:27:57 +0100

    fix(usagers): correction affichage procu & date message ephémère

--- packages/common/src/usager-options/classes/MailDelegate.class.ts
+++ packages/common/src/usager-options/classes/MailDelegate.class.ts
@@ -1,22 +1,29 @@
 import { createDate } from "../../_core";
 import { IMailDelegate } from "../interfaces";
-import { endOfDay, isBefore } from "date-fns";
+import { endOfDay, isAfter, isBefore } from "date-fns";
 
 export class MailDelegate implements IMailDelegate {
   public nom: string;
   public dateDebut: Date | null;
   public dateFin: Date | null;
   public isExpired?: boolean;
+  public isInactive?: boolean;
 
   constructor(options?: Partial<IMailDelegate>) {
     this.nom = options?.nom || "";
     this.dateDebut = createDate(options?.dateDebut);
     this.dateFin = createDate(options?.dateFin);
 
-    if (!this?.dateFin) {
+    const today = endOfDay(new Date());
+
+    if (this.dateDebut && !this.dateFin) {
       this.isExpired = false;
-    } else {
-      this.isExpired = isBefore(this.dateFin, endOfDay(new Date()));
+    } else if (this.dateDebut && this.dateFin) {
+      const startsInFuture = isAfter(this.dateDebut, today);
+      const alreadyEnded = isBefore(this.dateFin, today);
+
+      this.isExpired = alreadyEnded;
+      this.isInactive = startsInFuture;
     }
   }
 }
