1aa5b24a8ec8936de9fa24c42c4b3293f4735c74
Yassine R. <riffi.yassine@gmail.com>
2025-12-03 01:07:33 +0100

    fix(backend): fix unit tests

--- packages/backend/src/modules/portail-admin/services/structure-decision-email/structure-decision-email.service.spec.ts
+++ packages/backend/src/modules/portail-admin/services/structure-decision-email/structure-decision-email.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { StructureDecisionEmailService } from "./structure-decision-email.service";
+import { FileManagerService } from "../../../../util";
+import { MailsModule } from "../../../mails/mails.module";
 
 describe("StructureDecisionEmailService", () => {
   let service: StructureDecisionEmailService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StructureDecisionEmailService],
+      providers: [StructureDecisionEmailService, FileManagerService],
+      imports: [MailsModule],
     }).compile();
 
     service = module.get<StructureDecisionEmailService>(
--- packages/backend/src/modules/portail-admin/services/structure-decision/structure-decision.service.spec.ts
+++ packages/backend/src/modules/portail-admin/services/structure-decision/structure-decision.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { StructureDecisionService } from "./structure-decision.service";
+import { FileManagerService } from "../../../../util";
+import { MailsModule } from "../../../mails/mails.module";
 
 describe("StructureDecisionService", () => {
   let service: StructureDecisionService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StructureDecisionService],
+      providers: [StructureDecisionService, FileManagerService],
+      imports: [MailsModule],
     }).compile();
 
     service = module.get<StructureDecisionService>(StructureDecisionService);
