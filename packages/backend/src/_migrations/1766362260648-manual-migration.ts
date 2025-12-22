5e40ce80d0f81d616981a6106f2bea46d2b728cd
Yassine R. <riffi.yassine@gmail.com>
2025-12-23 00:13:54 +0100

    fix(backend): update dump for test

--- packages/backend/src/_migrations/1766362260648-manual-migration.ts
+++ packages/backend/src/_migrations/1766362260648-manual-migration.ts
@@ -1,50 +1,57 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
+import { domifaConfig } from "../config";
 
 export class ManualMigration1766362260648 implements MigrationInterface {
   name = "UpdateNationaliteOutreMerToFrance1766362260648";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // Liste des territoires d'outre-mer français à remplacer par "France"
-    const territoriesOutreMer = [
-      "Guadeloupe",
-      "Martinique",
-      "Guyane",
-      "Guyane française",
-      "La Réunion",
-      "Réunion",
-      "Mayotte",
-      "Saint-Pierre-et-Miquelon",
-      "Saint-Martin",
-      "Saint-Barthélemy",
-      "Polynésie française",
-      "Nouvelle-Calédonie",
-      "Wallis-et-Futuna",
-      "Wallis et Futuna",
-    ];
+    if (
+      domifaConfig().envId === "prod" ||
+      domifaConfig().envId === "local" ||
+      domifaConfig().envId === "preprod"
+    ) {
+      // Liste des territoires d'outre-mer français à remplacer par "France"
+      const territoriesOutreMer = [
+        "Guadeloupe",
+        "Martinique",
+        "Guyane",
+        "Guyane française",
+        "La Réunion",
+        "Réunion",
+        "Mayotte",
+        "Saint-Pierre-et-Miquelon",
+        "Saint-Martin",
+        "Saint-Barthélemy",
+        "Polynésie française",
+        "Nouvelle-Calédonie",
+        "Wallis-et-Futuna",
+        "Wallis et Futuna",
+      ];
 
-    // Mise à jour de la table usager
-    await queryRunner.query(
-      `
+      // Mise à jour de la table usager
+      await queryRunner.query(
+        `
       UPDATE usager
       SET nationalite = 'France'
       WHERE nationalite IN (${territoriesOutreMer
         .map((_, i) => `$${i + 1}`)
         .join(", ")})
     `,
-      territoriesOutreMer
-    );
+        territoriesOutreMer
+      );
 
-    // Mise à jour de la table usager_history_states
-    await queryRunner.query(
-      `
+      // Mise à jour de la table usager_history_states
+      await queryRunner.query(
+        `
       UPDATE usager_history_states
       SET nationalite = 'France'
       WHERE nationalite IN (${territoriesOutreMer
         .map((_, i) => `$${i + 1}`)
         .join(", ")})
     `,
-      territoriesOutreMer
-    );
+        territoriesOutreMer
+      );
+    }
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
