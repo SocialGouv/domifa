8033e450ef32fd6cef4e3ce2a5e8c86edbccc58d
Houssem Balty <balty.houssem@gmail.com>
2026-01-24 02:30:53 +0100

    feat(frontend): integrating dsfr faq

---
+++ packages/frontend/src/app/modules/general/components/faq/fragments/faq-video-tutorial/faq-video-tutorial.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  selector: "app-faq-video-tutorial",
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: "./faq-video-tutorial.component.html",
+})
+export class FaqVideoTutorialComponent {}
