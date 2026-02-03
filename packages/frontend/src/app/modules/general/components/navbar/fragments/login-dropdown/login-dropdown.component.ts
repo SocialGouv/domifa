0e5485ab29ddac1d62e0d96c20fd32cadad858e9
Yassine R. <riffi.yassine@gmail.com>
2026-02-03 23:04:08 +0100

    fix(frontend): fix menu

--- packages/frontend/src/app/modules/general/components/navbar/fragments/login-dropdown/login-dropdown.component.ts
+++ packages/frontend/src/app/modules/general/components/navbar/fragments/login-dropdown/login-dropdown.component.ts
@@ -1,25 +1,110 @@
-import { Component, Input } from "@angular/core";
+// login-dropdown.component.ts
+import { Component, Input, OnInit } from "@angular/core";
+import { DsfrLink } from "@edugouvfr/ngx-dsfr";
 
-import { UserStructure } from "@domifa/common";
 import { AuthService } from "../../../../../shared/services";
-import { MatomoTracker } from "ngx-matomo-client";
-/**
- * Login dropdown elements for desktop and easy access buttons
- */
+import { UserStructure } from "@domifa/common";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-login-dropdown",
   templateUrl: "./login-dropdown.component.html",
-  styleUrls: ["./login-dropdown.component.css"],
 })
-export class LoginDropdownComponent {
-  @Input() me: UserStructure | null;
+export class LoginDropdownComponent implements OnInit {
+  @Input({ required: true }) me: UserStructure | null = null;
+
+  public dsfrLinks: DsfrLink[] = [];
+  public userName = "";
   constructor(
     private readonly authService: AuthService,
-    public readonly matomoService: MatomoTracker
+    private readonly router: Router
   ) {}
 
-  public logout(): void {
-    this.authService.logoutFromBackend();
+  ngOnInit(): void {
+    this.buildLinks();
+    this.userName = this.me ? `${this.me.nom} ${this.me.prenom}` : "";
+  }
+
+  private buildLinks(): void {
+    this.dsfrLinks = [
+      {
+        label: "Nouveautés",
+        route: "/news",
+        icon: "fr-icon-newspaper-line",
+      },
+      {
+        label: "Gérer mon compte",
+        route: "/users/mon-compte",
+        icon: "fr-icon-admin-line",
+      },
+    ];
+
+    if (this.isAdminOrResponsable) {
+      this.dsfrLinks.push(
+        {
+          label: "Gérer les utilisateurs",
+          route: "/users/comptes",
+          icon: "fr-icon-group-line",
+        },
+        {
+          label: "Informations de la structure",
+          route: "/structures/edit",
+          icon: "fr-icon-community-line",
+        },
+        {
+          label: "Documents de la structure",
+          route: "/structures/documents",
+          icon: "fr-icon-file-line",
+        }
+      );
+
+      if (this.isSmsEnabled) {
+        this.dsfrLinks.push({
+          label: "Gérer l'envoi de SMS",
+          route: "/structures/sms",
+          icon: "fr-icon-smartphone-line",
+        });
+      }
+    }
+
+    this.dsfrLinks.push(
+      {
+        label: "Gérer le portail domicilié",
+        route: "/structures/portail-usager",
+        icon: "fr-icon-computer-line",
+      },
+      {
+        label: "Consulter les statistiques",
+        route: "/structure-stats",
+        icon: "fr-icon-bar-chart-box-line",
+      },
+      {
+        label: "Importer des domiciliés",
+        route: "/import",
+        icon: "fr-icon-upload-line",
+      }
+    );
+  }
+
+  onLinkSelect(link: DsfrLink): void {
+    if (link.route) {
+      this.router.navigate([link.route]);
+    }
+  }
+
+  logoutSelect(): void {
+    this.authService.logout();
+  }
+
+  get userEmail(): string {
+    return this.me?.email ?? "";
+  }
+
+  get isAdminOrResponsable(): boolean {
+    return this.me ? ["admin", "responsable"].includes(this.me.role) : false;
+  }
+
+  get isSmsEnabled(): boolean {
+    return this.me?.structure?.sms?.enabledByDomifa ?? false;
   }
 }
