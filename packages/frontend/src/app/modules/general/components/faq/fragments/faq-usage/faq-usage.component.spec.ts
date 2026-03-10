fd90de1dba96a313c2e24a6b2fb09a92f2a5c95a
Yassine R. <riffi.yassine@gmail.com>
2026-03-10 01:54:44 +0100

    fix(frontend): update tests

--- packages/frontend/src/app/modules/general/components/faq/fragments/faq-usage/faq-usage.component.spec.ts
+++ packages/frontend/src/app/modules/general/components/faq/fragments/faq-usage/faq-usage.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { FaqUsageComponent } from "./faq-usage.component";
+import { APP_BASE_HREF } from "@angular/common";
+import { provideHttpClient } from "@angular/common/http";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { MATOMO_INJECTORS } from "../../../../../../shared";
 
 describe("FaqUsageComponent", () => {
   let component: FaqUsageComponent;
