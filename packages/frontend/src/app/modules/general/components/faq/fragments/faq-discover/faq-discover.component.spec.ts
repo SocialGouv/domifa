7c139313d947da01497d2c92ae7ff7225ab1497d
Yassine R. <riffi.yassine@gmail.com>
2026-03-10 01:31:00 +0100

    fix(frontend): update unit tests

--- packages/frontend/src/app/modules/general/components/faq/fragments/faq-discover/faq-discover.component.spec.ts
+++ packages/frontend/src/app/modules/general/components/faq/fragments/faq-discover/faq-discover.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { FaqDiscoverComponent } from "./faq-discover.component";
+import { APP_BASE_HREF } from "@angular/common";
+import { provideHttpClient } from "@angular/common/http";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { MATOMO_INJECTORS } from "../../../../../../shared";
 
 describe("FaqDiscoverComponent", () => {
   let component: FaqDiscoverComponent;
@@ -8,7 +15,19 @@ describe("FaqDiscoverComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FaqDiscoverComponent],
+      imports: [
+        FaqDiscoverComponent,
+        NgbModule,
+        ReactiveFormsModule,
+        FormsModule,
+        RouterModule.forRoot([]),
+        ...MATOMO_INJECTORS,
+      ],
+      providers: [
+        provideHttpClient(),
+        { provide: APP_BASE_HREF, useValue: "/" },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FaqDiscoverComponent);
