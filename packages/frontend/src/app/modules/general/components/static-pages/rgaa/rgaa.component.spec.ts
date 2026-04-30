827255d56eafbd95a0ad90a78f090aa10705cfe8
Yassine R. <riffi.yassine@gmail.com>
2026-04-30 15:03:18 +0200

    fix(frontend): fix list

--- packages/frontend/src/app/modules/general/components/static-pages/rgaa/rgaa.component.spec.ts
+++ packages/frontend/src/app/modules/general/components/static-pages/rgaa/rgaa.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { RgaaComponent } from "./rgaa.component";
+import { RouterModule } from "@angular/router";
 
 describe("RgaaComponent", () => {
   let component: RgaaComponent;
@@ -8,7 +9,7 @@ describe("RgaaComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RgaaComponent],
+      imports: [RgaaComponent, RouterModule.forRoot([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RgaaComponent);
