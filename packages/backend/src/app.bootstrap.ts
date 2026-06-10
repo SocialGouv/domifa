bf8547ff2496dbd8c42e4366df33f597133e023b
Yassine R. <riffi.yassine@gmail.com>
2026-06-10 09:12:16 +0200

    fix(backend): fix import

--- packages/backend/src/app.bootstrap.ts
+++ packages/backend/src/app.bootstrap.ts
@@ -14,6 +14,7 @@ import { appTypeormManager } from "./database";
 
 import { AppSentryInterceptor } from "./util/sentry";
 import compression from "compression";
+import { setupLog, appLogger } from "./util";
 
 export async function tearDownApplication({
   app,
