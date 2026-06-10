ac82166a5355002103af9c4840100705c6e2d821
Yassine R. <riffi.yassine@gmail.com>
2026-06-10 09:16:54 +0200

    fix(backend): fix prod

--- packages/backend/src/util/index.ts
+++ packages/backend/src/util/index.ts
@@ -3,7 +3,6 @@ export * from "./encoding";
 export * from "./excel";
 export * from "./express";
 export * from "./file-manager";
-export * from "./filters";
 export * from "./functions";
 export * from "./logs";
 export * from "./phone";
