53526305cbd0c1f7f6acb70cde0836185c82621a
Yassine R. <riffi.yassine@gmail.com>
2026-06-30 17:03:25 +0200

    fix(backend): fix linter & imports

--- packages/backend/src/modules/otp/otp.module.ts
+++ packages/backend/src/modules/otp/otp.module.ts
@@ -3,12 +3,11 @@ import { forwardRef, Module } from "@nestjs/common";
 import { OtpGuard } from "./guards/otp.guard";
 import { OtpService } from "./services/otp.service";
 import { OtpEmailService } from "./services/otp-email.service";
-import { OtpCleaner } from "./services/otp-cleaner.service";
 import { MailsModule } from "../mails/mails.module";
 
 @Module({
   imports: [forwardRef(() => MailsModule)],
-  providers: [OtpService, OtpEmailService, OtpCleaner, OtpGuard],
+  providers: [OtpService, OtpEmailService, OtpGuard],
   exports: [OtpService, OtpGuard],
 })
 export class OtpModule {}
