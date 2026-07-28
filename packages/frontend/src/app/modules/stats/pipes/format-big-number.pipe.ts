b5e8389b67503bfbf020ee03eac9ccc0d6eab154
Abdelaziz SAKHI <abdelaziz.sakhi@gmail.com>
2026-07-28 11:26:50 +0200

    chore(frontend): update dsfr et fix formatBigNumberPipe condition

--- packages/frontend/src/app/modules/stats/pipes/format-big-number.pipe.ts
+++ packages/frontend/src/app/modules/stats/pipes/format-big-number.pipe.ts
@@ -3,6 +3,8 @@ import { Pipe, PipeTransform } from "@angular/core";
 @Pipe({ name: "formatBigNumber", standalone: true })
 export class FormatBigNumberPipe implements PipeTransform {
   public transform(nb: number): string {
+    if (!nb) return "";
+
     let strNb = nb.toString();
     const chunks = [];
 
