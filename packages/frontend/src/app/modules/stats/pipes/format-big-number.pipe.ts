e274ab7009ed2c34613416530f8e4ccacce104ea
Abdelaziz SAKHI <abdelaziz.sakhi@gmail.com>
2026-07-28 21:13:52 +0200

    chore(frontend): fix pr review

--- packages/frontend/src/app/modules/stats/pipes/format-big-number.pipe.ts
+++ packages/frontend/src/app/modules/stats/pipes/format-big-number.pipe.ts
@@ -3,7 +3,9 @@ import { Pipe, PipeTransform } from "@angular/core";
 @Pipe({ name: "formatBigNumber", standalone: true })
 export class FormatBigNumberPipe implements PipeTransform {
   public transform(nb: number): string {
-    if (!nb) return "";
+    if (!nb) {
+      return "";
+    }
 
     let strNb = nb.toString();
     const chunks = [];
