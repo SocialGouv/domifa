905c704ec31c66287c969918045c0b16da93c9e4
Abdelaziz SAKHI <abdelaziz.sakhi@gmail.com>
2026-08-05 17:30:34 +0200

    fix(admins): format big number pipe error

--- packages/portail-admins/src/app/modules/shared/pipes/format-big-number.pipe.ts
+++ packages/portail-admins/src/app/modules/shared/pipes/format-big-number.pipe.ts
@@ -5,6 +5,10 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class FormatBigNumberPipe implements PipeTransform {
   public transform(nb: number): string {
+    if (isNaN(nb)) {
+      return "";
+    }
+
     let strNb = nb.toString();
     const chunks = [];
 
