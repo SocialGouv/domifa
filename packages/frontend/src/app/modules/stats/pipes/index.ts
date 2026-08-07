2fd9d37fe40c146b57a20e72f5a9bd0fce4f7da7
Azizoou <abdelaziz.sakhi@gmail.com>
2026-08-07 14:15:49 +0200

    feat(frontend, admin): refactor big number pipe

--- packages/frontend/src/app/modules/stats/pipes/index.ts
+++
@@ -1,2 +0,0 @@
-// @index('./*.pipe.ts', f => `export * from '${f.path}'`)
-export * from "./format-big-number.pipe";
--- packages/portail-admins/src/app/modules/shared/pipes/index.ts
+++ packages/portail-admins/src/app/modules/shared/pipes/index.ts
@@ -1,5 +1,4 @@
 // @index('./*.pipe.ts', f => `export * from '${f.path}'`)
-export * from "./format-big-number.pipe";
 export * from "./format-phone-number.pipe";
 export * from "./full-name.pipe";
 export * from "./nl2br.pipe";
