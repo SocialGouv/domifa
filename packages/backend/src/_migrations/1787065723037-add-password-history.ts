b5e352849ac31280a03494bae2952c9d705d09b0
Azizoou <abdelaziz.sakhi@gmail.com>
2026-09-08 11:15:33 +0200

    Update packages/backend/src/_migrations/1787065723037-add-password-history.ts
    Co-authored-by: IterionBot <iterion-bot@proton.me>

--- packages/backend/src/_migrations/1787065723037-add-password-history.ts
+++ packages/backend/src/_migrations/1787065723037-add-password-history.ts
@@ -5,18 +5,12 @@ export class AddPasswordHistory1787065723037 implements MigrationInterface {
   name = "AddPasswordHistory1787065723037";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    if (
-      domifaConfig().envId === "prod" ||
-      domifaConfig().envId === "local" ||
-      domifaConfig().envId === "preprod"
-    ) {
-      await queryRunner.query(
-        `ALTER TABLE "user_structure_security" ADD "passwordHistory" jsonb NOT NULL DEFAULT '[]'`
-      );
-      await queryRunner.query(
-        `ALTER TABLE "user_supervisor_security" ADD "passwordHistory" jsonb NOT NULL DEFAULT '[]'`
-      );
-    }
+    await queryRunner.query(
+      `ALTER TABLE "user_structure_security" ADD COLUMN IF NOT EXISTS "passwordHistory" jsonb NOT NULL DEFAULT '[]'`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user_supervisor_security" ADD COLUMN IF NOT EXISTS "passwordHistory" jsonb NOT NULL DEFAULT '[]'`
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
